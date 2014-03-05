analogdevicesinc/libiio

commit ba6170e7d350bd51ab6dcd094841b45fcb9840f3

IIOD: Fixed two races
The first race was happening when a call to read_buffer was made
just a tiny bit after the read thread entered its shutdown phase;
in this case, iio_device_open() was sometimes called before
iio_device_close(), which returned an error.

The second race was happening when the read thread was signaling
back the waiting threads; those would instantly free the ThdEntry
structure, which was still needed in the read thread (to obtain the
address of the next element).

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/ops.c
@@ -121,6 +121,7 @@ static void * read_thd(void *d)
 
 		DEBUG("Reading %lu bytes from device\n", len);
 		ret = iio_device_read_raw(entry->dev, buf, len);
+
 		pthread_mutex_lock(&entry->thdlist_lock);
 		nb_samples = ret / sample_size;
 
@@ -147,16 +148,14 @@ static void * read_thd(void *d)
 			ret2 = write_all(buf, ret, thd->fd);
 			if (ret2 > 0)
 				thd->nb -= ret2 / sample_size;
-
-			if (ret2 < 0) {
-				SLIST_REMOVE(&entry->thdlist_head, thd,
-						ThdEntry, next);
+			if (ret2 < 0)
 				thd->err = ret2;
-				pthread_cond_signal(&thd->cond);
-			} else if (thd->nb == 0) {
+			else if (thd->nb == 0)
+				thd->err = 0;
+
+			if (ret2 < 0 || thd->nb == 0) {
 				SLIST_REMOVE(&entry->thdlist_head, thd,
 						ThdEntry, next);
-				thd->err = 0;
 				pthread_cond_signal(&thd->cond);
 			}
 		}
@@ -179,9 +178,10 @@ static void * read_thd(void *d)
 	DEBUG("Removing device %s from list\n",
 			iio_device_get_id(entry->dev));
 	SLIST_REMOVE(&devlist_head, entry, DevEntry, next);
-	pthread_mutex_unlock(&devlist_lock);
 
 	iio_device_close(entry->dev);
+	pthread_mutex_unlock(&devlist_lock);
+
 	pthread_mutex_destroy(&entry->thdlist_lock);
 	pthread_attr_destroy(&entry->attr);
 	free(entry);
@@ -199,6 +199,7 @@ static ssize_t read_buffer(struct parser_pdata *pdata, struct iio_device *dev,
 	ssize_t ret;
 
 	pthread_mutex_lock(&devlist_lock);
+
 	SLIST_FOREACH(e, &devlist_head, next) {
 		if (e->dev == dev) {
 			entry = e;
@@ -280,7 +281,15 @@ static ssize_t read_buffer(struct parser_pdata *pdata, struct iio_device *dev,
 	fflush(thd->fd);
 
 	ret = thd->err;
+
+	/* The read thread may still want to use thd->next;
+	 * locking the thread list's lock ensures that we will free
+	 * the 'thd' structure when it's not needed anymore */
+	pthread_mutex_lock(&entry->thdlist_lock);
 	free(thd);
+	pthread_mutex_unlock(&entry->thdlist_lock);
+
+	DEBUG("Exiting read_buffer\n");
 
 	if (ret < 0)
 		return ret;

commit 580599d6f5ac43a7fe8e75dd899ec3cfb579bcee

IIOD: Fix race introduced in ba6170e7
One of the fixes present in this previous commit also introduced
a race condition, which went under the radar as it is much less
likely to happen.

The fix was to lock the DevEntry's thdlist mutex before freeing
a ThdEntry, to ensure that the read thread is done using it.

However, between the moment when the thread was signaled and when
it tried to lock the thdlist mutex, the read thread could exit, if
it had no more clients for instance; and in the process, free its
DevEntry structure and the thdlist mutex it contains.

The real fix introduced in this commit removes the lock on the
thdlist mutex before freeing the ThdEntry. Instead, it makes sure
that the read thread will never access the "thd" structure from
the moment when the thread is signaled.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/ops.c
@@ -91,6 +91,7 @@ static void * read_thd(void *d)
 	while (true) {
 		char *buf;
 		unsigned long len, nb_samples = max_size;
+		struct ThdEntry *next_thd;
 
 		pthread_mutex_lock(&devlist_lock);
 
@@ -127,8 +128,15 @@ static void * read_thd(void *d)
 		pthread_mutex_lock(&entry->thdlist_lock);
 		nb_samples = ret / sample_size;
 
-		SLIST_FOREACH(thd, &entry->thdlist_head, next) {
+		/* We don't use SLIST_FOREACH here. As soon as a thread is
+		 * signaled, its "thd" structure might be freed;
+		 * SLIST_FOREACH would then cause a segmentation fault, as it
+		 * reads "thd" to get the address of the next element. */
+		for (thd = SLIST_FIRST(&entry->thdlist_head);
+				thd; thd = next_thd) {
 			size_t ret2;
+
+			next_thd = SLIST_NEXT(thd, next);
 
 			if (!thd->verbose) {
 				fprintf(thd->fd, "%li\n", (long) ret);
@@ -285,13 +293,7 @@ static ssize_t read_buffer(struct parser_pdata *pdata, struct iio_device *dev,
 	fflush(thd->fd);
 
 	ret = thd->err;
-
-	/* The read thread may still want to use thd->next;
-	 * locking the thread list's lock ensures that we will free
-	 * the 'thd' structure when it's not needed anymore */
-	pthread_mutex_lock(&entry->thdlist_lock);
 	free(thd);
-	pthread_mutex_unlock(&entry->thdlist_lock);
 
 	DEBUG("Exiting read_buffer\n");
 