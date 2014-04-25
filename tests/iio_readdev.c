f99b33ca1d93fc917ea56b243b16e0163d79140d
Paul Cercueil <paul.cercueil@analog.com>
2014-04-25 11:36:53 +0200

    iio_readdev: Update to match latest API
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- tests/iio_readdev.c
+++ tests/iio_readdev.c
@@ -204,7 +204,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	buffer = iio_device_create_buffer(dev, SAMPLES_PER_READ, false);
+	buffer = iio_device_create_buffer(dev, SAMPLES_PER_READ);
 	if (!buffer) {
 		ERROR("Unable to allocate buffer\n");
 		iio_context_destroy(ctx);
