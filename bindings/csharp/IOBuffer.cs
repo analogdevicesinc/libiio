ad204412f671bbd7075160a97e6f4b3b73dcdbd2
Paul Cercueil <paul.cercueil@analog.com>
2014-05-07 13:06:09 +0200

    bindings/csharp: Update IOBuffer to latest API
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- bindings/csharp/IOBuffer.cs
+++ bindings/csharp/IOBuffer.cs
@@ -10,12 +10,10 @@ namespace iio
     class IOBuffer
     {
         public IntPtr buf;
-        private bool is_output;
         private uint samples_count;
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
-        private static extern IntPtr iio_device_create_buffer(IntPtr dev, uint samples_count, 
-            [MarshalAs(UnmanagedType.I1)] bool is_output);
+        private static extern IntPtr iio_device_create_buffer(IntPtr dev, uint samples_count);
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern void iio_buffer_destroy(IntPtr buf);
@@ -26,12 +24,11 @@ namespace iio
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern int iio_buffer_push(IntPtr buf);
 
-        public IOBuffer(Device dev, uint samples_count, bool is_output = false)
+        public IOBuffer(Device dev, uint samples_count)
         {
-            this.is_output = is_output;
             this.samples_count = samples_count;
 
-            buf = iio_device_create_buffer(dev.dev, samples_count, is_output);
+            buf = iio_device_create_buffer(dev.dev, samples_count);
             if (buf == null)
                 throw new Exception("Unable to create buffer");
         }
@@ -43,9 +40,6 @@ namespace iio
 
         public void refill()
         {
-            if (this.is_output)
-                throw new Exception("Impossible to refill an output buffer");
-
             int err = iio_buffer_refill(this.buf);
             if (err < 0)
                 throw new Exception("Unable to refill buffer: err=" + err);
@@ -53,9 +47,6 @@ namespace iio
 
         public void push()
         {
-            if (!this.is_output)
-                throw new Exception("Impossible to push an input buffer");
-
             int err = iio_buffer_push(this.buf);
             if (err < 0)
                 throw new Exception("Unable to push buffer: err=" + err);
