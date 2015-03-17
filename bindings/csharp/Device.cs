a8940fccea71687db1881a0a493487cc097bb1b1
Paul Cercueil <paul.cercueil@analog.com>
2015-03-17 15:21:17 +0100

    C# bindings: Add a Device.debug_attrs field
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- bindings/csharp/Device.cs
+++ bindings/csharp/Device.cs
@@ -41,6 +41,38 @@ namespace iio
             }
         }
 
+        private class DeviceDebugAttr : Attr
+        {
+            private IntPtr dev;
+
+            [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
+            private static extern int iio_device_debug_attr_read(IntPtr dev, [In()] string name, [Out()] StringBuilder val, uint len);
+
+            [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
+            private static extern int iio_device_debug_attr_write(IntPtr dev, [In()] string name, [In()] string val);
+
+            public DeviceDebugAttr(IntPtr dev, string name) : base(name)
+            {
+                this.dev = dev;
+            }
+
+            public override string read()
+            {
+                StringBuilder builder = new StringBuilder(1024);
+                int err = iio_device_debug_attr_read(dev, name, builder, 1024);
+                if (err < 0)
+                    throw new Exception("Unable to read debug attribute " + err);
+                return builder.ToString();
+            }
+
+            public override void write(string str)
+            {
+                int err = iio_device_debug_attr_write(dev, name, str);
+                if (err < 0)
+                    throw new Exception("Unable to write debug attribute " + err);
+            }
+        }
+
         private Context ctx;
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -59,7 +91,13 @@ namespace iio
         private static extern uint iio_device_get_attrs_count(IntPtr dev);
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint iio_device_get_debug_attrs_count(IntPtr dev);
+
+        [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr iio_device_get_attr(IntPtr dev, uint index);
+
+        [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr iio_device_get_debug_attr(IntPtr dev, uint index);
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern int iio_device_get_trigger(IntPtr dev, IntPtr triggerptr);
@@ -73,7 +111,7 @@ namespace iio
         internal IntPtr dev;
         public readonly string id;
         public readonly string name;
-        public readonly List<Attr> attrs;
+        public readonly List<Attr> attrs, debug_attrs;
         public readonly List<Channel> channels;
 
         public Device(Context ctx, IntPtr dev)
@@ -82,14 +120,19 @@ namespace iio
             this.dev = dev;
             channels = new List<Channel>();
             attrs = new List<Attr>();
+            debug_attrs = new List<Attr>();
+
             uint nb_channels = iio_device_get_channels_count(dev),
-                nb_attrs = iio_device_get_attrs_count(dev);
+                nb_attrs = iio_device_get_attrs_count(dev),
+                nb_debug_attrs = iio_device_get_debug_attrs_count(dev);
 
             for (uint i = 0; i < nb_channels; i++)
                 channels.Add(new Channel(iio_device_get_channel(dev, i)));
 
             for (uint i = 0; i < nb_attrs; i++)
                 attrs.Add(new DeviceAttr(dev, Marshal.PtrToStringAnsi(iio_device_get_attr(dev, i))));
+            for (uint i = 0; i < nb_debug_attrs; i++)
+                debug_attrs.Add(new DeviceDebugAttr(dev, Marshal.PtrToStringAnsi(iio_device_get_debug_attr(dev, i))));
 
             id = Marshal.PtrToStringAnsi(iio_device_get_id(dev));
 
