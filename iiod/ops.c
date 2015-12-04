analogdevicesinc/libiio

commit 33613f272461fb643246c251683bd401eccc7901

IIOD: Improve parser input function
Previously, the parser would read the characters one by one from the
input file descriptor. This worked fine if the file descriptor had
in-kernel buffering.

However, when used with USB endpoints, reading one single character
would cause bad crashes in the kernel. In fact, with USB it is not valid
to read less bytes in one read() call than the amount of data that has
been transferred by the other hand.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/parser.y
@@ -42,29 +42,17 @@ int yylex_init_extra(void *d, yyscan_t *scanner);
 int yylex_destroy(yyscan_t yyscanner);
 
 void * yyget_extra(yyscan_t scanner);
+int yy_input(yyscan_t scanner, char *buf, size_t max_size);
 
 #define ECHO do { \
 		struct parser_pdata *pdata = yyget_extra(yyscanner); \
 		writefd(pdata, yytext, yyleng); \
 	} while (0)
 
-#define YY_INPUT(buf,result,max_size) { \
-		struct parser_pdata *pdata = yyget_extra(yyscanner); \
-		int c = '*'; \
-		size_t n; \
-		for ( n = 0; n < max_size && \
-			     readfd(pdata, &c, 1) > 0 && \
-				 c != '\n'; ++n ) \
-			buf[n] = (char) c; \
-		if ( c == '\n' ) { \
-			buf[n++] = (char) c; \
-		} else if ( c == EOF ) { \
-			ERROR( "input in flex scanner failed\n" ); \
-			n = 1; \
-			buf[0] = '\n'; \
-		} \
-		result = n; \
-	}
+#define YY_INPUT(buf,result,max_size) do { \
+		int res = yy_input(yyscanner, buf, max_size); \
+		result = res <= 0 ? YY_NULL : res; \
+	} while (0)
 }
 
 %define api.pure
@@ -413,4 +401,21 @@ void yyerror(yyscan_t scanner, const char *msg)
 		sprintf(buf, "%i\n", -EINVAL);
 		output(pdata, buf);
 	}
+}
+
+int yy_input(yyscan_t scanner, char *buf, size_t max_size)
+{
+	struct parser_pdata *pdata = yyget_extra(scanner);
+	int i, ret;
+
+	ret = readfd(pdata, buf, max_size);
+	if (ret < 0)
+		return -errno;
+	if (ret == 0)
+		return -EIO;
+
+	if (ret == max_size)
+		buf[max_size - 1] = '\0';
+
+	return ret;
 }

commit b7725a4de7d844be216a37f4a455d3385186b18c

IIOD: Fix bug introduced in 33613f2
The optimization introduced in commit 33613f2 ("IIOD: Improve parser
input function") came with a side effect. Since we were not reading
character-by-character anymore, there was no guarantee that the readfd()
function would only return the bytes composing the command, until the
trailing \n. In practice, that was always the case, except (in very
rare occasions) for with the WRITEBUF command, as the client might send
the data right after the command and before reading any error code. As
a result, the recv() call for the WRITEBUF command would read more
bytes than just the command, thus disturbing the communication and
crashing the IIO Daemon.

Instead of reverting to reading character-by-character, the new code
will first read one buffer of data without advancing the read offset,
search for the trailing \n, and advance the read offset by the correct
amount of bytes.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

iiod/ops.c
@@ -966,6 +966,42 @@ int set_buffers_count(struct parser_pdata *pdata,
 	return ret;
 }
 
+ssize_t read_line(struct parser_pdata *pdata, char *buf, size_t len)
+{
+	ssize_t ret;
+
+	if (pdata->fd_in_is_socket) {
+		/* First read from the socket, without advancing the
+		 * read offset */
+		ret = recv(pdata->fd_in, buf, len, MSG_NOSIGNAL | MSG_PEEK);
+		if (ret < 0) {
+			if (errno == ENOTSOCK)
+				pdata->fd_in_is_socket = false;
+		} else {
+			size_t i;
+
+			/* Lookup for the trailing \n */
+			for (i = 0; i < (size_t) ret && buf[i] != '\n'; i++);
+
+			/* No \n found? Just garbage data */
+			if (i == (size_t) ret) {
+				errno = EIO;
+				return -1;
+			}
+
+			/* Advance the read offset to the byte following
+			 * the \n */
+			ret = recv(pdata->fd_in, buf, i + 1,
+					MSG_NOSIGNAL | MSG_TRUNC);
+		}
+	}
+
+	if (!pdata->fd_in_is_socket)
+		ret = read(pdata->fd_in, buf, len);
+
+	return ret;
+}
+
 void interpreter(struct iio_context *ctx, int fd_in, int fd_out, bool verbose)
 {
 	yyscan_t scanner;

iiod/ops.h
@@ -69,6 +69,8 @@ int set_timeout(struct parser_pdata *pdata, unsigned int timeout);
 int set_buffers_count(struct parser_pdata *pdata,
 		struct iio_device *dev, long value);
 
+ssize_t read_line(struct parser_pdata *pdata, char *buf, size_t len);
+
 static __inline__ ssize_t writefd(struct parser_pdata *pdata,
 		const void *buf, size_t len)
 {

iiod/parser.y
@@ -408,7 +408,7 @@ int yy_input(yyscan_t scanner, char *buf, size_t max_size)
 	struct parser_pdata *pdata = yyget_extra(scanner);
 	int i, ret;
 
-	ret = readfd(pdata, buf, max_size);
+	ret = read_line(pdata, buf, max_size);
 	if (ret < 0)
 		return -errno;
 	if (ret == 0)