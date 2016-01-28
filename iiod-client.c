analogdevicesinc/libiio

commit 4f494bca7534a6903358541809a16f95f5582c06

iiod-client: get_trigger(): Read newline
When the server sends the trigger name it terminates it with a new lines.
Make sure to read it, otherwise we'll upset the internal state of the
client and subsequent commands will read incorrect values for the status
code.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

iiod-client.c
@@ -213,7 +213,7 @@ int iiod_client_get_trigger(struct iiod_client *client, int desc,
 
 	name_len = ret;
 
-	ret = (int) client->ops->read(pdata, desc, buf, name_len);
+	ret = (int) client->ops->read(pdata, desc, buf, name_len + 1);
 	if (ret < 0)
 		goto out_unlock;
 

commit 70cb1c8f96c9fcd76754a74e4106b5c4067e1619

iio-client: get_trigger(): Fix potential buffer overflow
Make sure the buffer has enough room for the terminating new-line
character.

Fixes: 4f494bca7534 ("iiod-client: get_trigger(): Read newline")
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

iiod-client.c
@@ -206,7 +206,7 @@ int iiod_client_get_trigger(struct iiod_client *client, int desc,
 	if (ret <= 0)
 		goto out_unlock;
 
-	if ((unsigned int) ret > sizeof(buf)) {
+	if ((unsigned int) ret > sizeof(buf) - 1) {
 		ret = -EIO;
 		goto out_unlock;
 	}