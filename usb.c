6c1bbd3c5a98a26e529ddb752e66b36d3981c9f7
Paul Cercueil <paul.cercueil@analog.com>
2016-02-09 14:00:40 +0100

    Usb backend: Dynamically reserve I/O endpoints
    When opening a IIO device, a I/O endpoint couple is now reserved
    for this IIO device.
    
    This has one serious benefit: two devices of the same type (RX or TX)
    won't share the same endpoint, and won't interlock themselves accessing
    the bus.
    
    However, it also has one serious drawback: it does not scale well. Only
    a certain number of IIO devices can be opened at the same time, until
    all the I/O endpoints are in use; then, any following attempt will
    return -EBUSY, until some IIO devices are closed.
    
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- usb.c
+++ usb.c
@@ -36,11 +36,12 @@
 /* Endpoint for non-streaming operations */
 #define EP_OPS		1
 
-/* Endpoint for input devices */
-#define EP_INPUT	2
+struct iio_usb_io_endpoint {
+	unsigned char address;
+	bool in_use;
 
-/* Endpoint for output devices */
-#define EP_OUTPUT	3
+	struct iio_mutex *lock;
+};
 
 struct iio_context_pdata {
 	libusb_context *ctx;
@@ -51,8 +52,11 @@ struct iio_context_pdata {
 	/* Lock for non-streaming operations */
 	struct iio_mutex *lock;
 
-	/* Locks for input/output devices */
-	struct iio_mutex *i_lock, *o_lock;
+	/* Lock for endpoint reservation */
+	struct iio_mutex *ep_lock;
+
+	struct iio_usb_io_endpoint *io_endpoints;
+	unsigned int nb_io_endpoints;
 
 	unsigned int timeout_ms;
 };
@@ -115,6 +119,41 @@ static unsigned int usb_calculate_remote_timeout(unsigned int timeout)
 	return timeout / 2;
 }
 
+static int usb_reserve_ep_unlocked(const struct iio_device *dev)
+{
+	struct iio_context_pdata *pdata = dev->ctx->pdata;
+	unsigned int i;
+
+	for (i = 0; i < pdata->nb_io_endpoints; i++) {
+		struct iio_usb_io_endpoint *ep = &pdata->io_endpoints[i];
+
+		if (!ep->in_use) {
+			ep->in_use = true;
+
+			dev->pdata->ep = ep->address;
+			dev->pdata->lock = ep->lock;
+			return 0;
+		}
+	}
+
+	return -EBUSY;
+}
+
+static void usb_free_ep_unlocked(const struct iio_device *dev)
+{
+	struct iio_context_pdata *pdata = dev->ctx->pdata;
+	unsigned int i;
+
+	for (i = 0; i < pdata->nb_io_endpoints; i++) {
+		struct iio_usb_io_endpoint *ep = &pdata->io_endpoints[i];
+
+		if (ep->lock == dev->pdata->lock) {
+			ep->in_use = false;
+			return;
+		}
+	}
+}
+
 static int usb_open(const struct iio_device *dev,
 		size_t samples_count, bool cyclic)
 {
@@ -122,10 +161,16 @@ static int usb_open(const struct iio_device *dev,
 	struct iio_device_pdata *pdata = dev->pdata;
 	int ret = -EBUSY;
 
-	iio_mutex_lock(pdata->lock);
+	iio_mutex_lock(ctx_pdata->ep_lock);
 
 	if (pdata->opened)
 		goto out_unlock;
+
+	ret = usb_reserve_ep_unlocked(dev);
+	if (ret)
+		goto out_unlock;
+
+	iio_mutex_lock(pdata->lock);
 
 	ret = iiod_client_open_unlocked(ctx_pdata->iiod_client,
 			pdata->ep, dev, samples_count, cyclic);
@@ -140,24 +185,35 @@ static int usb_open(const struct iio_device *dev,
 
 	pdata->opened = !ret;
 
-out_unlock:
 	iio_mutex_unlock(pdata->lock);
+
+	if (ret)
+		usb_free_ep_unlocked(dev);
+
+out_unlock:
+	iio_mutex_unlock(ctx_pdata->ep_lock);
 	return ret;
 }
 
 static int usb_close(const struct iio_device *dev)
 {
+	struct iio_context_pdata *ctx_pdata = dev->ctx->pdata;
 	struct iio_device_pdata *pdata = dev->pdata;
 	int ret = -EBADF;
 
-	iio_mutex_lock(pdata->lock);
-	if (pdata->opened) {
-		ret = iiod_client_close_unlocked(dev->ctx->pdata->iiod_client,
-				pdata->ep, dev);
-		pdata->opened = false;
-	}
+	iio_mutex_lock(ctx_pdata->ep_lock);
+	if (!pdata->opened)
+		goto out_unlock;
 
-	iio_mutex_unlock(pdata->lock);
+	iio_mutex_lock(pdata->lock);
+	ret = iiod_client_close_unlocked(ctx_pdata->iiod_client,
+			pdata->ep, dev);
+	pdata->opened = false;
+
+	usb_free_ep_unlocked(dev);
+
+out_unlock:
+	iio_mutex_unlock(ctx_pdata->ep_lock);
 	return ret;
 }
 
@@ -250,9 +306,16 @@ static int usb_set_timeout(struct iio_context *ctx, unsigned int timeout)
 
 static void usb_shutdown(struct iio_context *ctx)
 {
+	unsigned int i;
+
 	iio_mutex_destroy(ctx->pdata->lock);
-	iio_mutex_destroy(ctx->pdata->o_lock);
-	iio_mutex_destroy(ctx->pdata->i_lock);
+	iio_mutex_destroy(ctx->pdata->ep_lock);
+
+	for (i = 0; i < ctx->pdata->nb_io_endpoints; i++)
+		if (ctx->pdata->io_endpoints[i].lock)
+			iio_mutex_destroy(ctx->pdata->io_endpoints[i].lock);
+	if (ctx->pdata->io_endpoints)
+		free(ctx->pdata->io_endpoints);
 
 	iiod_client_destroy(ctx->pdata->iiod_client);
 
@@ -308,10 +371,46 @@ static const struct iiod_client_ops usb_iiod_client_ops = {
 	.read_line = read_data_sync,
 };
 
+static int usb_count_io_eps(const struct libusb_interface_descriptor *iface)
+{
+	unsigned int eps = iface->bNumEndpoints;
+	unsigned int i, curr;
+
+	/* Check that for a number of endpoints X provided by the interface, we
+	 * have the input and output endpoints in the address range [1, ... X/2]
+	 * and that each input endpoint has a corresponding output endpoint at
+	 * the same address. */
+
+	if (eps < 2 || eps % 2)
+		return -EINVAL;
+
+	for (curr = 1; curr < (eps / 2) + 1; curr++) {
+		bool found_in = false, found_out = false;
+
+		for (i = 0; !found_in && i < eps; i++)
+			found_in = iface->endpoint[i].bEndpointAddress ==
+				LIBUSB_ENDPOINT_IN | curr;
+		if (!found_in)
+			return -EINVAL;
+
+		for (i = 0; !found_out && i < eps; i++)
+			found_out = iface->endpoint[i].bEndpointAddress ==
+				LIBUSB_ENDPOINT_OUT | curr;
+		if (!found_out)
+			return -EINVAL;
+	}
+
+	/* -1: we reserve the first I/O endpoint couple for global operations */
+	return (int) curr - 1;
+}
+
 struct iio_context * usb_create_context(unsigned short vid, unsigned short pid)
 {
 	libusb_context *usb_ctx;
+	libusb_device *usb_dev;
 	libusb_device_handle *hdl;
+	const struct libusb_interface_descriptor *iface;
+	struct libusb_config_descriptor *conf_desc;
 	struct iio_context *ctx;
 	struct iio_context_pdata *pdata;
 	unsigned int i;
@@ -331,18 +430,11 @@ struct iio_context * usb_create_context(unsigned short vid, unsigned short pid)
 		goto err_free_pdata;
 	}
 
-	pdata->i_lock = iio_mutex_create();
-	if (!pdata->i_lock) {
+	pdata->ep_lock = iio_mutex_create();
+	if (!pdata->ep_lock) {
 		ERROR("Unable to create mutex\n");
 		ret = -ENOMEM;
 		goto err_destroy_mutex;
-	}
-
-	pdata->o_lock = iio_mutex_create();
-	if (!pdata->o_lock) {
-		ERROR("Unable to create mutex\n");
-		ret = -ENOMEM;
-		goto err_destroy_i_mutex;
 	}
 
 	pdata->iiod_client = iiod_client_new(pdata, pdata->lock,
@@ -350,7 +442,7 @@ struct iio_context * usb_create_context(unsigned short vid, unsigned short pid)
 	if (!pdata->iiod_client) {
 		ERROR("Unable to create IIOD client\n");
 		ret = -errno;
-		goto err_destroy_o_mutex;
+		goto err_destroy_ep_mutex;
 	}
 
 	ret = libusb_init(&usb_ctx);
@@ -376,13 +468,62 @@ struct iio_context * usb_create_context(unsigned short vid, unsigned short pid)
 		goto err_libusb_close;
 	}
 
+	usb_dev = libusb_get_device(hdl);
+
+	ret = libusb_get_active_config_descriptor(usb_dev, &conf_desc);
+	if (ret) {
+		ret = -(int) libusb_to_errno(ret);
+		ERROR("Unable to get config descriptor: %i\n", ret);
+		goto err_libusb_close;
+	}
+
+	iface = &conf_desc->interface[0].altsetting[0];
+
+	ret = usb_count_io_eps(iface);
+	if (ret < 0) {
+		ERROR("Invalid configuration of endpoints\n");
+		goto err_free_config_descriptor;
+	}
+
+	pdata->nb_io_endpoints = ret;
+
+	DEBUG("Found %hhu usable i/o endpoints\n", pdata->nb_io_endpoints);
+
+	if (pdata->nb_io_endpoints) {
+		pdata->io_endpoints = calloc(pdata->nb_io_endpoints,
+				sizeof(*pdata->io_endpoints));
+		if (!pdata->io_endpoints) {
+			ERROR("Unable to allocate endpoints\n");
+			ret = -ENOMEM;
+			goto err_free_config_descriptor;
+		}
+
+		for (i = 0; i < pdata->nb_io_endpoints; i++) {
+			struct iio_usb_io_endpoint *ep =
+				&pdata->io_endpoints[i];
+
+			/* +2: endpoints start at number 1, and we skip the
+			 * endpoint #1 that we reserve for global operations */
+			ep->address = i + 2;
+
+			ep->lock = iio_mutex_create();
+			if (!ep->lock) {
+				ERROR("Unable to create mutex\n");
+				ret = -ENOMEM;
+				goto err_free_endpoints;
+			}
+		}
+	}
+
 	pdata->ctx = usb_ctx;
 	pdata->hdl = hdl;
 	pdata->timeout_ms = DEFAULT_TIMEOUT_MS;
 
 	ctx = iiod_client_create_context(pdata->iiod_client, EP_OPS);
 	if (!ctx)
-		goto err_libusb_close;
+		goto err_free_endpoints;
+
+	libusb_free_config_descriptor(conf_desc);
 
 	ctx->name = "usb";
 	ctx->ops = &usb_ops;
@@ -404,14 +545,6 @@ struct iio_context * usb_create_context(unsigned short vid, unsigned short pid)
 		}
 
 		dev->pdata->is_tx = iio_device_is_tx(dev);
-
-		if (dev->pdata->is_tx) {
-			dev->pdata->lock = pdata->o_lock;
-			dev->pdata->ep = EP_OUTPUT;
-		} else {
-			dev->pdata->lock = pdata->i_lock;
-			dev->pdata->ep = EP_INPUT;
-		}
 	}
 
 	return ctx;
@@ -421,16 +554,22 @@ err_context_destroy:
 	errno = -ret;
 	return NULL;
 
+err_free_endpoints:
+	for (i = 0; i < pdata->nb_io_endpoints; i++)
+		if (pdata->io_endpoints[i].lock)
+			iio_mutex_destroy(pdata->io_endpoints[i].lock);
+	if (pdata->io_endpoints)
+		free(pdata->io_endpoints);
+err_free_config_descriptor:
+	libusb_free_config_descriptor(conf_desc);
 err_libusb_close:
 	libusb_close(hdl);
 err_libusb_exit:
 	libusb_exit(usb_ctx);
 err_destroy_iiod_client:
 	iiod_client_destroy(pdata->iiod_client);
-err_destroy_o_mutex:
-	iio_mutex_destroy(pdata->o_lock);
-err_destroy_i_mutex:
-	iio_mutex_destroy(pdata->i_lock);
+err_destroy_ep_mutex:
+	iio_mutex_destroy(pdata->ep_lock);
 err_destroy_mutex:
 	iio_mutex_destroy(pdata->lock);
 err_free_pdata:
