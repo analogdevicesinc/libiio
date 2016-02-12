analogdevicesinc/libiio

commit d09449001032181069645ff893e901b52ee55d7e

Network backend: Speed up read_line() function on Linux
It now uses the same trick as in IIOD: it first fetches the data, locate
the trailing \n, then advance the read pointer to the byte following the
new line character.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

network.c
@@ -955,6 +955,24 @@ static ssize_t network_read_line(struct iio_context_pdata *pdata,
 		int desc, char *dst, size_t len)
 {
 	size_t i;
+#ifdef __linux__
+	ssize_t ret;
+
+	/* First read from the socket without advancing the read offset */
+	ret = recv(desc, dst, len, MSG_PEEK);
+	if (ret < 0)
+		return ret;
+
+	/* Lookup for the trailing \n */
+	for (i = 0; i < (size_t) ret && dst[i] != '\n'; i++);
+
+	/* No \n found? Just garbage data */
+	if (i == (size_t) ret)
+		return -EIO;
+
+	/* Advance the read offset to the byte following the \n */
+	return recv(desc, dst, i + 1, MSG_TRUNC);
+#else
 	bool found = false;
 
 	for (i = 0; i < len - 1; i++) {
@@ -971,6 +989,7 @@ static ssize_t network_read_line(struct iio_context_pdata *pdata,
 
 	dst[i] = '\0';
 	return (ssize_t) i;
+#endif
 }
 
 static struct iiod_client_ops network_iiod_client_ops = {

commit cd19bc1147417546c30424ed655f17ca7358aabc

network: read_line(): Propagate correct error codes
The upper layers expect to see the error code and not just -1 in case on an
error.

Fixes: d09449001032 ("Network backend: Speed up read_line() function on Linux")
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

network.c
@@ -939,7 +939,7 @@ static ssize_t network_read_line(struct iio_context_pdata *pdata,
 	/* First read from the socket without advancing the read offset */
 	ret = recv(desc, dst, len, MSG_PEEK);
 	if (ret < 0)
-		return ret;
+		return -errno;
 
 	/* Lookup for the trailing \n */
 	for (i = 0; i < (size_t) ret && dst[i] != '\n'; i++);
@@ -949,7 +949,10 @@ static ssize_t network_read_line(struct iio_context_pdata *pdata,
 		return -EIO;
 
 	/* Advance the read offset to the byte following the \n */
-	return recv(desc, dst, i + 1, MSG_TRUNC);
+	ret = recv(desc, dst, i + 1, MSG_TRUNC);
+	if (ret < 0)
+		return -errno;
+	return ret;
 #else
 	bool found = false;
 