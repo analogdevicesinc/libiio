analogdevicesinc/libiio

commit 93b59500754c11497e1c63f9e07172383e276bcb

Local backend: Check if device is ready before swapping buffers
This change should avoid the program to hang if the DMA is locked.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

local.c
@@ -212,11 +212,11 @@ static int set_channel_name(struct iio_channel *chn)
 	return 0;
 }
 
-static int device_check_ready(const struct iio_device *dev, bool do_write)
+static int device_check_ready(const struct iio_device *dev, short events)
 {
 	struct pollfd pollfd = {
 		.fd = dev->pdata->fd,
-		.events = do_write ? POLLOUT : POLLIN,
+		.events = events,
 	};
 	int ret;
 
@@ -230,7 +230,7 @@ static int device_check_ready(const struct iio_device *dev, bool do_write)
 		return -ETIMEDOUT;
 	if (pollfd.revents & POLLNVAL)
 		return -EBADF;
-	if (!(pollfd.revents & (do_write ? POLLOUT : POLLIN)))
+	if (!(pollfd.revents & events))
 		return -EIO;
 	return 0;
 }
@@ -324,7 +324,7 @@ static ssize_t local_read(const struct iio_device *dev,
 	if (ret < 0)
 		return ret;
 
-	ret = device_check_ready(dev, false);
+	ret = device_check_ready(dev, POLLIN);
 	if (ret < 0)
 		return ret;
 
@@ -350,7 +350,7 @@ static ssize_t local_write(const struct iio_device *dev,
 		return -EPERM;
 
 	if (pdata->cyclic) {
-		ret = device_check_ready(dev, true);
+		ret = device_check_ready(dev, POLLOUT);
 		if (ret < 0)
 			return ret;
 
@@ -366,7 +366,7 @@ static ssize_t local_write(const struct iio_device *dev,
 	/* In cyclic mode, the buffer must be enabled after writing the samples.
 	 * In non-cyclic mode, it must be enabled before writing the samples. */
 	if (!pdata->cyclic) {
-		ret = device_check_ready(dev, true);
+		ret = device_check_ready(dev, POLLOUT);
 		if (ret < 0)
 			return ret;
 
@@ -404,6 +404,10 @@ static ssize_t local_get_buffer(const struct iio_device *dev,
 		return -EBADF;
 	if (!addr_ptr)
 		return -EINVAL;
+
+	ret = (ssize_t) device_check_ready(dev, POLLIN | POLLOUT);
+	if (ret < 0)
+		return ret;
 
 	if (pdata->last_dequeued >= 0) {
 		struct block *last_block = &pdata->blocks[pdata->last_dequeued];

commit 6a5367fcfae571da4de1cb0623b9ecdfb1323f80

local: local_get_buffer(): Fix deadlock when only one block is use
Upon the first invocation local_get_buffer() will dequeue a fresh buffer
from the kernel and then return. The application will fill this block with
data and local_get_buffer() will be called again. Upon the second
invocation local_get_buffer() will enqueue the block dequeued in the
previous run and then dequeue the next block when it becomes available.

Before enqueuing the block local_get_buffer() calls device_check_ready() to
make wait for a new block to become available for the dequeue operation
later on. When using only one buffer this will never happen since the only
buffer is currently already dequeued and local_get_buffer() will wait until
a timeout occurs and then return an error.

To fix the issue move the device_check_ready() call after the block enqueue
operation and right before the dequeue operation. When only one block is
available it will first be passed back to kernel before waiting for it to
be come available for dequeuing.

Fixes: 93b59500754c ("Local backend: Check if device is ready before swapping buffers"
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

local.c
@@ -430,10 +430,6 @@ static ssize_t local_get_buffer(const struct iio_device *dev,
 	if (!addr_ptr)
 		return -EINVAL;
 
-	ret = (ssize_t) device_check_ready(dev, POLLIN | POLLOUT);
-	if (ret < 0)
-		return ret;
-
 	if (pdata->last_dequeued >= 0) {
 		struct block *last_block = &pdata->blocks[pdata->last_dequeued];
 
@@ -459,6 +455,10 @@ static ssize_t local_get_buffer(const struct iio_device *dev,
 			return (ssize_t) last_block->bytes_used;
 		}
 	}
+
+	ret = (ssize_t) device_check_ready(dev, POLLIN | POLLOUT);
+	if (ret < 0)
+		return ret;
 
 	memset(&block, 0, sizeof(block));
 	ret = (ssize_t) ioctl(f, BLOCK_DEQUEUE_IOCTL, &block);