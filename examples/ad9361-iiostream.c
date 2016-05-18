541c6a96afa4257983c553721669fb0da6daae32
Lars-Peter Clausen <lars@metafoo.de>
2016-05-18 19:41:41 +0200

    ad9361-iiostream: Increase default sample rate
    The current default sample rate is below the minimum sample rate of the
    AD9361, which will cause the application to abort with an error if no
    sample rate is specified. Increase the default so it is above the minimum.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- examples/ad9361-iiostream.c
+++ examples/ad9361-iiostream.c
@@ -188,13 +188,13 @@ int main (int argc, char **argv)
 
 	// RX stream config
 	rxcfg.bw_hz = MHZ(2);   // 2 MHz rf bandwidth
-	rxcfg.fs_hz = MHZ(2);   // 2 MS/s rx sample rate
+	rxcfg.fs_hz = MHZ(2.5);   // 2.5 MS/s rx sample rate
 	rxcfg.lo_hz = GHZ(2.5); // 2.5 GHz rf frequency
 	rxcfg.rfport = "A_BALANCED"; // port A (select for rf freq.)
 
 	// TX stream config
 	txcfg.bw_hz = MHZ(1.5); // 1.5 MHz rf bandwidth
-	txcfg.fs_hz = MHZ(2);   // 2 MS/s tx sample rate
+	txcfg.fs_hz = MHZ(2.5);   // 2.5 MS/s tx sample rate
 	txcfg.lo_hz = GHZ(2.5); // 2.5 GHz rf frequency
 	txcfg.rfport = "A"; // port A (select for rf freq.)
 
