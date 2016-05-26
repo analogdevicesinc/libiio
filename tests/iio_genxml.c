4d4643790597ac254a78578ceba674817ab1c77f
Paul Cercueil <paul.cercueil@analog.com>
2016-05-26 11:22:17 +0200

    iio_genxml: Fix MSVC warning
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- tests/iio_genxml.c
+++ tests/iio_genxml.c
@@ -21,6 +21,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifndef _WIN32
+#define _strdup strdup
+#endif
+
 #define MY_NAME "iio_genxml"
 
 enum backend {
@@ -120,7 +124,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	xml = strdup(iio_context_get_xml(ctx));
+	xml = _strdup(iio_context_get_xml(ctx));
 	if (!xml) {
 		iio_context_destroy(ctx);
 		return EXIT_FAILURE;
