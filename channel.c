analogdevicesinc/libiio

commit 47585f13d4b814cfb16799786493c54afeafa881

channel: Add API for getting the channel modifier
Some applications are interested in the channel modifier. Rather than
making them to have to parse the channel ID introduce a new function that
returns the channel modifier as iio_modifier enum.

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

channel.c
@@ -53,6 +53,70 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_PH] = "ph",
 };
 
+static const char * const modifier_names[] = {
+	[IIO_MOD_X] = "x",
+	[IIO_MOD_Y] = "y",
+	[IIO_MOD_Z] = "z",
+	[IIO_MOD_X_AND_Y] = "x&y",
+	[IIO_MOD_X_AND_Z] = "x&z",
+	[IIO_MOD_Y_AND_Z] = "y&z",
+	[IIO_MOD_X_AND_Y_AND_Z] = "x&y&z",
+	[IIO_MOD_X_OR_Y] = "x|y",
+	[IIO_MOD_X_OR_Z] = "x|z",
+	[IIO_MOD_Y_OR_Z] = "y|z",
+	[IIO_MOD_X_OR_Y_OR_Z] = "x|y|z",
+	[IIO_MOD_ROOT_SUM_SQUARED_X_Y] = "sqrt(x^2+y^2)",
+	[IIO_MOD_SUM_SQUARED_X_Y_Z] = "x^2+y^2+z^2",
+	[IIO_MOD_LIGHT_BOTH] = "both",
+	[IIO_MOD_LIGHT_IR] = "ir",
+	[IIO_MOD_LIGHT_CLEAR] = "clear",
+	[IIO_MOD_LIGHT_RED] = "red",
+	[IIO_MOD_LIGHT_GREEN] = "green",
+	[IIO_MOD_LIGHT_BLUE] = "blue",
+	[IIO_MOD_QUATERNION] = "quaternion",
+	[IIO_MOD_TEMP_AMBIENT] = "ambient",
+	[IIO_MOD_TEMP_OBJECT] = "object",
+	[IIO_MOD_NORTH_MAGN] = "from_north_magnetic",
+	[IIO_MOD_NORTH_TRUE] = "from_north_true",
+	[IIO_MOD_NORTH_MAGN_TILT_COMP] = "from_north_magnetic_tilt_comp",
+	[IIO_MOD_NORTH_TRUE_TILT_COMP] = "from_north_true_tilt_comp",
+	[IIO_MOD_RUNNING] = "running",
+	[IIO_MOD_JOGGING] = "jogging",
+	[IIO_MOD_WALKING] = "walking",
+	[IIO_MOD_STILL] = "still",
+	[IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z] = "sqrt(x^2+y^2+z^2)",
+	[IIO_MOD_I] = "i",
+	[IIO_MOD_Q] = "q",
+	[IIO_MOD_CO2] = "co2",
+	[IIO_MOD_VOC] = "voc",
+};
+
+/*
+ * Looks for a IIO channel modifier at the beginning of the string s. If a
+ * modifier was found the symbolic constant (IIO_MOD_*) is returned, otherwise
+ * IIO_NO_MOD is returned. If a modifier was found len_p will be updated with
+ * the length of the modifier.
+ */
+unsigned int find_channel_modifier(const char *s, size_t *len_p)
+{
+	unsigned int i;
+	size_t len;
+
+	for (i = 0; i < ARRAY_SIZE(modifier_names); i++) {
+		if (!modifier_names[i])
+			continue;
+		len = strlen(modifier_names[i]);
+		if (strncmp(s, modifier_names[i], len) == 0 &&
+				(s[len] == '\0' || s[len] == '_')) {
+			if (len_p)
+				*len_p = len;
+			return i;
+		}
+	}
+
+	return IIO_NO_MOD;
+}
+
 /*
  * Initializes all auto-detected fields of the channel struct. Must be called
  * after the channel has been otherwise fully initialized.
@@ -61,8 +125,10 @@ void iio_channel_init_finalize(struct iio_channel *chn)
 {
 	unsigned int i;
 	size_t len;
+	char *mod;
 
 	chn->type = IIO_CHAN_TYPE_UNKNOWN;
+	chn->modifier = IIO_NO_MOD;
 
 	for (i = 0; i < ARRAY_SIZE(iio_chan_type_name_spec); i++) {
 		len = strlen(iio_chan_type_name_spec[i]);
@@ -73,6 +139,23 @@ void iio_channel_init_finalize(struct iio_channel *chn)
 			continue;
 
 		chn->type = i;
+	}
+
+	mod = strchr(chn->id, '_');
+	if (!mod)
+		return;
+
+	mod++;
+
+	for (i = 0; i < ARRAY_SIZE(modifier_names); i++) {
+		len = strlen(modifier_names[i]);
+		if (strncmp(modifier_names[i], mod, len) != 0)
+			continue;
+		/* Modifier must be followed by a '_' */
+		if (mod[len] != '_')
+			continue;
+
+		chn->modifier = i;
 		break;
 	}
 }
@@ -219,6 +302,11 @@ bool iio_channel_is_output(const struct iio_channel *chn)
 bool iio_channel_is_scan_element(const struct iio_channel *chn)
 {
 	return chn->is_scan_element;
+}
+
+enum iio_modifier iio_channel_get_modifier(const struct iio_channel *chn)
+{
+	return chn->modifier;
 }
 
 enum iio_chan_type iio_channel_get_type(const struct iio_channel *chn)

iio-private.h
@@ -89,43 +89,6 @@ static inline void *zalloc(size_t size)
 	return calloc(1, size);
 }
 
-enum iio_modifier {
-	IIO_NO_MOD,
-	IIO_MOD_X,
-	IIO_MOD_Y,
-	IIO_MOD_Z,
-	IIO_MOD_X_AND_Y,
-	IIO_MOD_X_AND_Z,
-	IIO_MOD_Y_AND_Z,
-	IIO_MOD_X_AND_Y_AND_Z,
-	IIO_MOD_X_OR_Y,
-	IIO_MOD_X_OR_Z,
-	IIO_MOD_Y_OR_Z,
-	IIO_MOD_X_OR_Y_OR_Z,
-	IIO_MOD_LIGHT_BOTH,
-	IIO_MOD_LIGHT_IR,
-	IIO_MOD_ROOT_SUM_SQUARED_X_Y,
-	IIO_MOD_SUM_SQUARED_X_Y_Z,
-	IIO_MOD_LIGHT_CLEAR,
-	IIO_MOD_LIGHT_RED,
-	IIO_MOD_LIGHT_GREEN,
-	IIO_MOD_LIGHT_BLUE,
-	IIO_MOD_QUATERNION,
-	IIO_MOD_TEMP_AMBIENT,
-	IIO_MOD_TEMP_OBJECT,
-	IIO_MOD_NORTH_MAGN,
-	IIO_MOD_NORTH_TRUE,
-	IIO_MOD_NORTH_MAGN_TILT_COMP,
-	IIO_MOD_NORTH_TRUE_TILT_COMP,
-	IIO_MOD_RUNNING,
-	IIO_MOD_JOGGING,
-	IIO_MOD_WALKING,
-	IIO_MOD_STILL,
-	IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z,
-	IIO_MOD_I,
-	IIO_MOD_Q,
-};
-
 struct iio_backend_ops {
 	struct iio_context * (*clone)(const struct iio_context *ctx);
 	ssize_t (*read)(const struct iio_device *dev, void *dst, size_t len,
@@ -199,6 +162,7 @@ struct iio_channel {
 	struct iio_data_format format;
 	char *name, *id;
 	long index;
+	enum iio_modifier modifier;
 	enum iio_chan_type type;
 
 	struct iio_channel_attr *attrs;
@@ -295,6 +259,7 @@ __api ssize_t iio_device_get_sample_size_mask(const struct iio_device *dev,
 		const uint32_t *mask, size_t words);
 
 void iio_channel_init_finalize(struct iio_channel *chn);
+unsigned int find_channel_modifier(const char *s, size_t *len_p);
 
 #undef __api
 

iio.h
@@ -111,6 +111,52 @@ enum iio_chan_type {
 	IIO_CHAN_TYPE_UNKNOWN = INT_MAX
 };
 
+/**
+ * @enum iio_modifier
+ * @brief IIO channel modifier
+ *
+ * In a addition to a type a IIO channel can optionally have a channel modifier
+ * further specifying the data type of of the channel.
+ */
+enum iio_modifier {
+	IIO_NO_MOD,
+	IIO_MOD_X,
+	IIO_MOD_Y,
+	IIO_MOD_Z,
+	IIO_MOD_X_AND_Y,
+	IIO_MOD_X_AND_Z,
+	IIO_MOD_Y_AND_Z,
+	IIO_MOD_X_AND_Y_AND_Z,
+	IIO_MOD_X_OR_Y,
+	IIO_MOD_X_OR_Z,
+	IIO_MOD_Y_OR_Z,
+	IIO_MOD_X_OR_Y_OR_Z,
+	IIO_MOD_LIGHT_BOTH,
+	IIO_MOD_LIGHT_IR,
+	IIO_MOD_ROOT_SUM_SQUARED_X_Y,
+	IIO_MOD_SUM_SQUARED_X_Y_Z,
+	IIO_MOD_LIGHT_CLEAR,
+	IIO_MOD_LIGHT_RED,
+	IIO_MOD_LIGHT_GREEN,
+	IIO_MOD_LIGHT_BLUE,
+	IIO_MOD_QUATERNION,
+	IIO_MOD_TEMP_AMBIENT,
+	IIO_MOD_TEMP_OBJECT,
+	IIO_MOD_NORTH_MAGN,
+	IIO_MOD_NORTH_TRUE,
+	IIO_MOD_NORTH_MAGN_TILT_COMP,
+	IIO_MOD_NORTH_TRUE_TILT_COMP,
+	IIO_MOD_RUNNING,
+	IIO_MOD_JOGGING,
+	IIO_MOD_WALKING,
+	IIO_MOD_STILL,
+	IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z,
+	IIO_MOD_I,
+	IIO_MOD_Q,
+	IIO_MOD_CO2,
+	IIO_MOD_VOC,
+};
+
 /* ---------------------------------------------------------------------------*/
 /* ------------------------- Scan functions ----------------------------------*/
 /** @defgroup Scan Functions for scanning available contexts
@@ -963,6 +1009,13 @@ __api void * iio_channel_get_data(const struct iio_channel *chn);
  * @param chn A pointer to an iio_channel structure
  * @return The type of the channel */
 __api __pure enum iio_chan_type iio_channel_get_type(
+		const struct iio_channel *chn);
+
+
+/** @brief Get the modifier type of the given channel
+ * @param chn A pointer to an iio_channel structure
+ * @return The modifier type of the channel */
+__api __pure enum iio_modifier iio_channel_get_modifier(
 		const struct iio_channel *chn);
 
 

local.c
@@ -97,60 +97,6 @@ static const char * const device_attrs_blacklist[] = {
 	"uevent",
 };
 
-static const char * const modifier_names[] = {
-	[IIO_MOD_X] = "x",
-	[IIO_MOD_Y] = "y",
-	[IIO_MOD_Z] = "z",
-	[IIO_MOD_ROOT_SUM_SQUARED_X_Y] = "sqrt(x^2+y^2)",
-	[IIO_MOD_SUM_SQUARED_X_Y_Z] = "x^2+y^2+z^2",
-	[IIO_MOD_LIGHT_BOTH] = "both",
-	[IIO_MOD_LIGHT_IR] = "ir",
-	[IIO_MOD_LIGHT_CLEAR] = "clear",
-	[IIO_MOD_LIGHT_RED] = "red",
-	[IIO_MOD_LIGHT_GREEN] = "green",
-	[IIO_MOD_LIGHT_BLUE] = "blue",
-	[IIO_MOD_QUATERNION] = "quaternion",
-	[IIO_MOD_TEMP_AMBIENT] = "ambient",
-	[IIO_MOD_TEMP_OBJECT] = "object",
-	[IIO_MOD_NORTH_MAGN] = "from_north_magnetic",
-	[IIO_MOD_NORTH_TRUE] = "from_north_true",
-	[IIO_MOD_NORTH_MAGN_TILT_COMP] = "from_north_magnetic_tilt_comp",
-	[IIO_MOD_NORTH_TRUE_TILT_COMP] = "from_north_true_tilt_comp",
-	[IIO_MOD_RUNNING] = "running",
-	[IIO_MOD_JOGGING] = "jogging",
-	[IIO_MOD_WALKING] = "walking",
-	[IIO_MOD_STILL] = "still",
-	[IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z] = "sqrt(x^2+y^2+z^2)",
-	[IIO_MOD_I] = "i",
-	[IIO_MOD_Q] = "q",
-};
-
-/*
- * Looks for a IIO channel modifier at the beginning of the string s. If a
- * modifier was found the symbolic constant (IIO_MOD_*) is returned, otherwise
- * IIO_NO_MOD is returned. If a modifier was found len_p will be update with the
- * length of the modifier.
- */
-static unsigned int find_modifier(const char *s, size_t *len_p)
-{
-	unsigned int i;
-	size_t len;
-
-	for (i = 0; i < ARRAY_SIZE(modifier_names); i++) {
-		if (!modifier_names[i])
-			continue;
-		len = strlen(modifier_names[i]);
-		if (strncmp(s, modifier_names[i], len) == 0 &&
-				(s[len] == '\0' || s[len] == '_')) {
-			if (len_p)
-				*len_p = len;
-			return i;
-		}
-	}
-
-	return IIO_NO_MOD;
-}
-
 static int ioctl_nointr(int fd, unsigned long request, void *data)
 {
 	int ret;
@@ -1007,7 +953,7 @@ static bool is_channel(const char *attr, bool strict)
 	if (*(ptr - 1) >= '0' && *(ptr - 1) <= '9')
 		return true;
 
-	if (find_modifier(ptr + 1, NULL) != IIO_NO_MOD)
+	if (find_channel_modifier(ptr + 1, NULL) != IIO_NO_MOD)
 		return true;
 	return false;
 }
@@ -1019,7 +965,7 @@ static char * get_channel_id(const char *attr)
 
 	attr = strchr(attr, '_') + 1;
 	ptr = strchr(attr, '_');
-	if (find_modifier(ptr + 1, &len) != IIO_NO_MOD)
+	if (find_channel_modifier(ptr + 1, &len) != IIO_NO_MOD)
 		ptr += len + 1;
 
 	res = malloc(ptr - attr + 1);
@@ -1037,7 +983,7 @@ static char * get_short_attr_name(struct iio_channel *chn, const char *attr)
 	size_t len;
 
 	ptr = strchr(ptr, '_') + 1;
-	if (find_modifier(ptr, &len) != IIO_NO_MOD)
+	if (find_channel_modifier(ptr, &len) != IIO_NO_MOD)
 		ptr += len + 1;
 
 	if (chn->name) {
@@ -1253,7 +1199,7 @@ static unsigned int is_global_attr(struct iio_channel *chn, const char *attr)
 		return 0;
 	}
 
-	if (find_modifier(chn->id + len + 1, NULL) != IIO_NO_MOD)
+	if (find_channel_modifier(chn->id + len + 1, NULL) != IIO_NO_MOD)
 		return 1;
 
 	return 0;

commit c93caed266da58aecd82371248fa709833d04086

iio_channel_init_finalize(): Skip NULL entries in modifier_names
The modifier_names array contains NULL entries. Those need to be skipped
otherwise the function will cause a NULL pointer dereference.

Fixes: 47585f13d4b8 ("channel: Add API for getting the channel modifier")
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

channel.c
@@ -148,6 +148,8 @@ void iio_channel_init_finalize(struct iio_channel *chn)
 	mod++;
 
 	for (i = 0; i < ARRAY_SIZE(modifier_names); i++) {
+		if (!modifier_names[i])
+			continue;
 		len = strlen(modifier_names[i]);
 		if (strncmp(modifier_names[i], mod, len) != 0)
 			continue;