analogdevicesinc/libiio

commit 1128c2d476df48441b87a9eb5e24a26e4d1bfc9a

Network backend: Fix network_read_line() function
The network_read_line() function is optimized so that it can read a
whole line of text in just two system calls, instead of reading
character-by-character until the end-of-line character is found.

However, it did not handle the fact that recv() can return a number of
bytes smaller than the actual response received from IIOD. In that case
it was mis-detecting the beginning of the response line as garbage,
since the \n was not found.

This commit makes sure that the network_read_line() function will
call again recv() if the \n is not found and we still have space in
the destination buffer.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

network.c
@@ -1279,27 +1279,47 @@ static ssize_t network_read_data(struct iio_context_pdata *pdata,
 static ssize_t network_read_line(struct iio_context_pdata *pdata,
 		void *io_data, char *dst, size_t len)
 {
+	bool found = false;
 	size_t i;
 #ifdef __linux__
 	struct iio_network_io_context *io_ctx = io_data;
 	ssize_t ret;
+	size_t bytes_read = 0;
 
-	ret = network_recv(io_ctx, dst, len, MSG_PEEK);
-	if (ret < 0)
-		return ret;
+	do {
+		size_t to_trunc;
 
-	/* Lookup for the trailing \n */
-	for (i = 0; i < (size_t) ret && dst[i] != '\n'; i++);
+		ret = network_recv(io_ctx, dst, len, MSG_PEEK);
+		if (ret < 0)
+			return ret;
 
-	/* No \n found? Just garbage data */
-	if (i == (size_t) ret)
+		/* Lookup for the trailing \n */
+		for (i = 0; i < (size_t) ret && dst[i] != '\n'; i++);
+		found = i < (size_t) ret;
+
+		len -= ret;
+		dst += ret;
+		bytes_read += i;
+
+		if (found)
+			to_trunc = i + 1;
+		else
+			to_trunc = (size_t) ret;
+
+		/* Advance the read offset to the byte following the \n if
+		 * found, or after the last charater read otherwise */
+		ret = network_recv(io_ctx, NULL, to_trunc, MSG_TRUNC);
+		if (ret < 0)
+			return ret;
+
+		bytes_read += to_trunc;
+	} while (!found && len);
+
+	if (!found)
 		return -EIO;
-
-	/* Advance the read offset to the byte following the \n */
-	return network_recv(io_ctx, dst, i + 1, MSG_TRUNC);
+	else
+		return bytes_read;
 #else
-	bool found = false;
-
 	for (i = 0; i < len - 1; i++) {
 		ssize_t ret = network_read_data(pdata, io_data, dst + i, 1);
 

commit 13cebe0e39ac837b55beafb9a8ca5aee22502de9

Network backend: Fix read_line return value
This fixes a bug introduced in commit 1128c2d.
The bytes_read variable was incremented at two different places.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

network.c
@@ -1299,7 +1299,6 @@ static ssize_t network_read_line(struct iio_context_pdata *pdata,
 
 		len -= ret;
 		dst += ret;
-		bytes_read += i;
 
 		if (found)
 			to_trunc = i + 1;