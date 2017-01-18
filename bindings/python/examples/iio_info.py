9e30414ab69881dff213acaeb72e45ae60e771d9
Iztok Jeras <iztok.jeras@redpitaya.com>
2017-01-18 15:13:18 +0100

    Python3: added parentheses around prinf contents

--- bindings/python/examples/iio_info.py
+++ bindings/python/examples/iio_info.py
@@ -18,42 +18,42 @@ import iio
 def main():
 	ctx = iio.Context()
 
-	print 'Library version: %u.%u (git tag: %s)' % iio.version
+	print ('Library version: %u.%u (git tag: %s)' % iio.version)
 
-	print 'IIO context created: ' + ctx.name
-	print 'Backend version: %u.%u (git tag: %s)' % ctx.version
-	print 'Backend description string: ' + ctx.description
+	print ('IIO context created: ' + ctx.name)
+	print ('Backend version: %u.%u (git tag: %s)' % ctx.version)
+	print ('Backend description string: ' + ctx.description)
 
-	print 'IIO context has %u devices:' % len(ctx.devices)
+	print ('IIO context has %u devices:' % len(ctx.devices))
 
 	for dev in ctx.devices:
-		print '\t' + dev.id + ': ' + dev.name
+		print ('\t' + dev.id + ': ' + dev.name)
 
 		if dev is iio.Trigger:
-			print 'Found trigger! Rate: %u Hz' % dev.frequency
+			print ('Found trigger! Rate: %u Hz' % dev.frequency)
 
-		print '\t\t%u channels found:' % len(dev.channels)
+		print ('\t\t%u channels found:' % len(dev.channels))
 
 		for chn in dev.channels:
-			print '\t\t\t%s: %s (%s)' % (chn.id, chn.name or "", 'output' if chn.output else 'input')
+			print ('\t\t\t%s: %s (%s)' % (chn.id, chn.name or "", 'output' if chn.output else 'input'))
 
 			if len(chn.attrs) != 0:
-				print '\t\t\t%u channel-specific attributes found:' % len(chn.attrs)
+				print ('\t\t\t%u channel-specific attributes found:' % len(chn.attrs))
 
 			for attr in chn.attrs:
-				print '\t\t\t\t' + attr + ', value: ' + chn.attrs[attr].value
+				print ('\t\t\t\t' + attr + ', value: ' + chn.attrs[attr].value)
 
 		if len(dev.attrs) != 0:
-			print '\t\t%u device-specific attributes found:' % len(dev.attrs)
+			print ('\t\t%u device-specific attributes found:' % len(dev.attrs))
 
 		for attr in dev.attrs:
-			print '\t\t\t' + attr + ', value: ' + dev.attrs[attr].value
+			print ('\t\t\t' + attr + ', value: ' + dev.attrs[attr].value)
 
 		if len(dev.debug_attrs) != 0:
-			print '\t\t%u debug attributes found:' % len(dev.debug_attrs)
+			print ('\t\t%u debug attributes found:' % len(dev.debug_attrs))
 
 		for attr in dev.debug_attrs:
-			print '\t\t\t' + attr + ', value: ' + dev.debug_attrs[attr].value
+			print ('\t\t\t' + attr + ', value: ' + dev.debug_attrs[attr].value)
 
 if __name__ == '__main__':
 	main()
