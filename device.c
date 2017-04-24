analogdevicesinc/libiio

commit 4792fa3a005165597afeed3700831302c256d1d6

Use channel number instead of channel index in mask
By 'channel number', I mean the index at which the 'iio_channel'
structure appears in the parent iio_device.

Most of the cases, this is generally the same as the index of the
channel (returned by iio_channel_get_index()), since the channels are
ordered by index in the parent iio_device.

This change is required to support the (rare) cases where the
iio_channels share the same index. Previously, those channels would
set the exact same bit in the channel mask, which would confuse IIOD
about which channels were enabled.

This change should not break the ABI between libiio and IIOD, because
the channels are sorted the exact same way on both ends.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

device.c
@@ -427,6 +427,7 @@ ssize_t iio_device_get_sample_size_mask(const struct iio_device *dev,
 {
 	ssize_t size = 0;
 	unsigned int i;
+	const struct iio_channel *prev = NULL;
 
 	if (words != (dev->nb_channels + 31) / 32)
 		return -EINVAL;
@@ -438,10 +439,13 @@ ssize_t iio_device_get_sample_size_mask(const struct iio_device *dev,
 
 		if (chn->index < 0)
 			break;
-		if (!TEST_BIT(mask, chn->index))
+		if (!TEST_BIT(mask, chn->number))
 			continue;
-		if (i > 0 && chn->index == dev->channels[i - 1]->index)
+
+		if (prev && chn->index == prev->index) {
+			prev = chn;
 			continue;
+		}
 
 		if (size % length)
 			size += 2 * length - (size % length);

channel.c
@@ -395,19 +395,19 @@ const struct iio_data_format * iio_channel_get_data_format(
 bool iio_channel_is_enabled(const struct iio_channel *chn)
 {
 	return chn->index >= 0 && chn->dev->mask &&
-		TEST_BIT(chn->dev->mask, chn->index);
+		TEST_BIT(chn->dev->mask, chn->number);
 }
 
 void iio_channel_enable(struct iio_channel *chn)
 {
 	if (chn->is_scan_element && chn->index >= 0 && chn->dev->mask)
-		SET_BIT(chn->dev->mask, chn->index);
+		SET_BIT(chn->dev->mask, chn->number);
 }
 
 void iio_channel_disable(struct iio_channel *chn)
 {
 	if (chn->index >= 0 && chn->dev->mask)
-		CLEAR_BIT(chn->dev->mask, chn->index);
+		CLEAR_BIT(chn->dev->mask, chn->number);
 }
 
 void free_channel(struct iio_channel *chn)

context.c
@@ -223,6 +223,9 @@ static void reorder_channels(struct iio_device *dev)
 			}
 		}
 	} while (found);
+
+	for (i = 0; i < dev->nb_channels; i++)
+		dev->channels[i]->number = i;
 }
 
 int iio_context_init(struct iio_context *ctx)

iio-private.h
@@ -173,6 +173,8 @@ struct iio_channel {
 
 	struct iio_channel_attr *attrs;
 	unsigned int nb_attrs;
+
+	unsigned int number;
 };
 
 struct iio_device {

commit 263bd0802cb405f1025b83dbce468481b6516cd3

Fix broken iio_device_get_sample_size with same-index channels
This fixes a bug introduced in 4792fa3.

Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

device.c
@@ -451,6 +451,8 @@ ssize_t iio_device_get_sample_size_mask(const struct iio_device *dev,
 			size += 2 * length - (size % length);
 		else
 			size += length;
+
+		prev = chn;
 	}
 	return size;
 }