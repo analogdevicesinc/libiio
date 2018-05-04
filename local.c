analogdevicesinc/libiio

commit e6754cb84a8b9d1ab5b79bb44dca18bcffde3236

local: Return error when no blocks could be allocated
The BLOCK_ALLOC_IOCTL will not fail if not enough memory is available to
allocate all requested memory. Instead it will allocate as much as it can
and report how much was allocated.

This includes the case where no memory could be allocated. A buffer without
any backing memory doesn't make much sense though, so error out in this
case.

This also fixes a deadlock in libiio where it would be waiting for a block
to become available during buffer creation (which would never happen
without any blocks).

Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

local.c
@@ -784,6 +784,11 @@ static int enable_high_speed(const struct iio_device *dev)
 		goto err_freemem;
 	}
 
+	if (req.count == 0) {
+		ret = -ENOMEM;
+		goto err_block_free;
+	}
+
 	/* We might get less blocks than what we asked for */
 	pdata->nb_blocks = req.count;
 
@@ -817,6 +822,7 @@ static int enable_high_speed(const struct iio_device *dev)
 err_munmap:
 	for (; i > 0; i--)
 		munmap(pdata->addrs[i - 1], pdata->blocks[i - 1].size);
+err_block_free:
 	ioctl_nointr(fd, BLOCK_FREE_IOCTL, 0);
 err_freemem:
 	free(pdata->addrs);
@@ -881,7 +887,12 @@ static int local_open(const struct iio_device *dev,
 	pdata->cyclic_buffer_enqueued = false;
 	pdata->buffer_enabled = false;
 	pdata->samples_count = samples_count;
-	pdata->is_high_speed = !enable_high_speed(dev);
+
+	ret = enable_high_speed(dev);
+	if (ret < 0 && ret != -ENOSYS)
+		goto err_close;
+
+	pdata->is_high_speed = !ret;
 
 	if (!pdata->is_high_speed) {
 		unsigned long size = samples_count * pdata->nb_blocks;

commit 351aac39710f3baf6330dee3ded71edafec7a437

local: Fix low-speed mode
Commit e6754cb84a8b ("local: Return error when no blocks could be
allocated") made the wrong assumption that the BLOCK_ALLOC_IOCTL will
return the ENOSYS error when high-speed mode is not supported. This is not
the case though and the ioctl returns the EINVAL error instead.

This breaks low-speed support since libiio now assumes that high-speed mode
is always supported and something went wrong when trying to allocate the
high-speed buffer and never tries to fallback to low-speed mode.

Unfortunately it is not possible to distinguish between a EINVAL that means
high-speed mode is not supported and a EINVAL that means high-speed mode is
supported but something went wrong when allocating the high-speed buffer.

To avoid this use the BLOCK_FREE_IOCTL to check whether high-speed mode is
supported or not. If high-speed mode is supported BLOCK_FREE_IOCTL will
never return an error, if it is not supported it will return the EINVAL
error.

Fixes: e6754cb84a8b ("local: Return error when no blocks could be allocated")
Reviewed-by: Richard Tresidder <rtresidd@tresar-electronics.com.au>
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

local.c
@@ -816,6 +816,16 @@ static int enable_high_speed(const struct iio_device *dev)
 	unsigned int i;
 	int ret, fd = pdata->fd;
 
+	/*
+	 * For the BLOCK_ALLOC_IOCTL ioctl it is not possible to distingush
+	 * between an error during the allocation (e.g. incorrect size) or
+	 * whether the high-speed interface is not supported. BLOCK_FREE_IOCTL does
+	 * never fail if the device supports the high-speed interface, so we use it
+	 * here. Calling it when no blocks are allocated the ioctl has no effect.
+	 */
+	ret = ioctl_nointr(fd, BLOCK_FREE_IOCTL, NULL);
+	if (ret < 0)
+		return -ENOSYS;
 
 	if (pdata->cyclic) {
 		nb_blocks = 1;