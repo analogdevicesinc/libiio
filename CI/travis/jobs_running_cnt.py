bac1d5ef4ed70b422a55cd7c93b20ba943ec7631
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-07-31 15:21:21 +0300

    .travis.yml,CI/travis/after_deploy: trigger other builds after libiio
    Let's start a "pipeline", where the libiio build triggers a
    `libad9361-iio`, which will trigger other builds as well.
    
    We're getting to a point where the lack of automation is starting to hinder
    us a bit and changing things in `libiio` are not visible along the way,
    until it gets a bit late.
    
    There's a Python script that each job would run, to check if it's the last
    job (in the parallel CI run), and the last job, will trigger the next
    build.
    There's no better way at the moment to do this, without re-factoring the
    entire `.travis.yml` file to work with build-stages (a new feature that's a
    bit of work to get working properly with our current setup).
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ CI/travis/jobs_running_cnt.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+import sys
+import urllib2
+import json
+
+# This is pretty constant, but allow it to be overriden via env-var
+url = os.getenv('TRAVIS_API_URL', 'https://api.travis-ci.org')
+
+if (not url.lower().startswith("https://")):
+    print (0)
+    sys.exit(0)
+
+ci_token = os.getenv('TRAVIS_API_TOKEN')
+build_id = os.getenv('TRAVIS_BUILD_ID')
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Travis-API-Version': "3",
+    'Authorization': "token {0}".format(ci_token)
+}
+
+# Codacy's bandit linter may complain that we haven't validated
+# this URL for permitted schemes; we have validated this a few lines above
+req = urllib2.Request("{0}/build/{1}/jobs".format(url, build_id),
+		      headers=headers)
+
+response = urllib2.urlopen(req).read()
+json_r = json.loads(response.decode('utf-8'))
+
+jobs_running = 0
+for job in json_r['jobs']:
+    if (job['state'] == 'started'):
+        jobs_running += 1
+
+print (jobs_running)
