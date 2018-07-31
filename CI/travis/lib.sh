f125524d536322c2a2bb74025f03563ba843cab0
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-07-31 15:21:21 +0300

    .travis.yml,CI/travis/after_deploy: trigger other builds after libiio
    Let's start a "pipeline", where the libiio build triggers a
    `libad9361-iio`, which will trigger other builds as well.
    
    We're getting to a point where the lack of automation is starting to hinder
    us a bit and changing things in `libiio` are not visible along the way,
    until it gets a bit late.
    
    There's a Python script that each job would run, to check if it's the last
    job (in the parallel CI run), and the last job, will trigger the next
    build.
    There's no better way at the moment to do this, without re-factoring the
    entire `.travis.yml` file to work with build-stages (a new feature that's a
    bit of work to get working properly with our current setup).
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/lib.sh
+++ CI/travis/lib.sh
@@ -1,5 +1,96 @@
 #!/bin/sh -xe
 
+export TRAVIS_API_URL="https://api.travis-ci.org"
+
+pipeline_branch() {
+	local branch=$1
+
+	[ -n "$branch" ] || return 1
+
+	# master is a always a pipeline branch
+	[ "$branch" = "master" ] && return 0
+
+	# Turn off tracing for a while ; log can fill up here
+	set +x
+	# Check if branch name is 20XX_RY where:
+	#   XX - 14 to 99 /* wooh, that's a lot of years */
+	#   Y  - 1 to 9   /* wooh, that's a lot of releases per year */
+	for year in $(seq 2014 2099) ; do
+		for rel_num in $(seq 1 9) ; do
+			[ "$branch" = "${year}_R${rel_num}" ] && \
+				return 0
+		done
+	done
+	set -x
+
+	return 1
+}
+
+should_trigger_next_builds() {
+	local branch="$1"
+
+	[ -z "${COVERITY_SCAN_PROJECT_NAME}"] || return 1
+
+	# These Travis-CI vars have to be non-empty
+	[ -n "$TRAVIS_PULL_REQUEST" ] || return 1
+	[ -n "$branch" ] || return 1
+	set +x
+	[ -n "$TRAVIS_API_TOKEN" ] || return 1
+	set -x
+
+	# Has to be a non-pull-request
+	[ "$TRAVIS_PULL_REQUEST" = "false" ] || return 1
+
+	pipeline_branch "$branch" || return 1
+
+	if [ -f CI/travis/jobs_running_cnt.py ] ; then
+		local python_script=CI/travis/jobs_running_cnt.py
+	elif [ -f build/jobs_running_cnt.py ] ; then
+		local python_script=build/jobs_running_cnt.py
+	else
+		echo "Could not find 'jobs_running_cnt.py'"
+		return 1
+	fi
+
+	local jobs_cnt=$(python $python_script)
+
+	# Trigger next job if we are the last job running
+	[ "$jobs_cnt" = "1" ]
+}
+
+trigger_build() {
+	local repo_slug="$1"
+	local branch="$2"
+
+	[ -n "$repo_slug" ] || return 1
+	[ -n "$branch" ] || return 1
+
+	local body="{
+		\"request\": {
+			\"branch\":\"$branch\"
+		}
+	}"
+
+	# Turn off tracing here (shortly)
+	set +x
+	curl -s -X POST \
+		-H "Content-Type: application/json" \
+		-H "Accept: application/json" \
+		-H "Travis-API-Version: 3" \
+		-H "Authorization: token $TRAVIS_API_TOKEN" \
+		-d "$body" \
+		https://api.travis-ci.org/repo/$repo_slug/requests
+	set -x
+}
+
+trigger_adi_build() {
+	local adi_repo="$1"
+	local branch="$2"
+
+	[ -n "$adi_repo" ] || return 1
+	trigger_build "analogdevicesinc%2F$adi_repo" "$branch"
+}
+
 get_ldist() {
 	case "$(uname)" in
 	Linux*)
