f60f95789705da4943ce6c862bdcfd57711d7cf7
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-09-27 14:56:10 +0300

    cmake/LinuxPackaging.cmake: use non-dev packages to .deb dep list
    When `libiio` is to be installed it doesn't necessarily need the
    development versions of these libs.
    These libs are statically defined though.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- cmake/LinuxPackaging.cmake
+++ cmake/LinuxPackaging.cmake
@@ -100,7 +100,7 @@ if(DEB_DETECT_DEPENDENCIES AND DPKG_CMD AND DPKGQ_CMD)
 		${CPACK_DEBIAN_PACKAGE_DEPENDS})
 else()
 	# assume everything is turned on, and running on a modern OS
-	set(CPACK_DEBIAN_PACKAGE_DEPENDS "libaio-dev (>= 0.3.109), libavahi-client-dev (>= 0.6.31), libavahi-common-dev (>= 0.6.31), libc6-dev (>= 2.19), libusb-1.0-0-dev (>= 2:1.0.17), libxml2-dev (>= 2.9.1)")
+	set(CPACK_DEBIAN_PACKAGE_DEPENDS "libaio (>= 0.3.109), libavahi-client (>= 0.6.31), libavahi-common (>= 0.6.31), libc6 (>= 2.19), libusb-1.0-0 (>= 2:1.0.17), libxml2 (>= 2.9.1)")
 	message(STATUS "Using default dependencies for packaging")
 endif()
 
