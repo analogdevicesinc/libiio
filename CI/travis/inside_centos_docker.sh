1cbfb20f22fab3246e19d1bf3357bd5884223e05
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-09-27 16:01:23 +0300

    CI/travis/inside_centos_docker.sh: hack/patch CPackRPM.cmake for CentOS 7
    The version of `cmake` in CentOS 7 is broken for packaging RPMs.
    It creates RPMs that conflict with other RPMs, because it adds base-system
    directories to the `libiio` RPM (e.g. /usr/sbin, /lib, etc).
    
    This is because the CPackRPM.cmake module has an error in the logic.
    To workaround it (until fixed), we're patching it before running the cmake
    build. This happens in a docker env, so it should be fine.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/inside_centos_docker.sh
+++ CI/travis/inside_centos_docker.sh
@@ -8,6 +8,26 @@ yum -y groupinstall 'Development Tools'
 yum -y install cmake libxml2-devel libusb1-devel doxygen libaio-devel \
 	avahi-devel bzip2 gzip rpm rpm-build
 
+# FIXME: cmake in CentOS 7 is broken, so we need to patch it for now
+# Remove this when it's fixed; but make sure we're doing this in Travis-CI context
+if [ "$TRAVIS_CI" == "travis-ci" ] ; then
+	if [ "$OS_VERSION" == "7" ] ; then
+		patch -p1 <<-EOF
+--- a/usr/share/cmake/Modules/CPackRPM.cmake
++++ b/usr/share/cmake/Modules/CPackRPM.cmake
+@@ -703,7 +703,7 @@ if(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST)
+    message("CPackRPM:Debug: CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST= \${CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST}")
+  endif()
+   foreach(_DIR \${CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST})
+-    list(APPEND _RPM_DIRS_TO_OMIT "-o;-path;.\${_DIR}")
++    list(APPEND _RPM_DIRS_TO_OMIT "-o -path \${_DIR}")
+   endforeach()
+ endif()
+ if (CPACK_RPM_PACKAGE_DEBUG)
+		EOF
+	fi
+fi
+
 # Check we're in Travis-CI; the only place where this context is valid
 # It makes sure, users won't shoot themselves in the foot while running this script
 if [ "$TRAVIS_CI" == "travis-ci" ] ; then
