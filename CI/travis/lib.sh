421d1d0340785f0fee9683183c85a0eb96080479
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-11-22 15:10:32 +0200

    CI/travis/deploy: move the send() func to lib.sh
    The logic is pretty re-usable on it's own and can be used in other
    deployment scripts, so move it to `lib.sh`.
    
    Since it's there, it needs a renaming to something that is a bit more
    explicit than `send()`.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/lib.sh
+++ CI/travis/lib.sh
@@ -55,3 +55,68 @@ brew_install_or_upgrade() {
 		shift
 	done
 }
+
+upload_file_to_swdownloads() {
+	if [ "$#" -ne 3 ] ; then
+		echo "skipping deployment of something"
+		echo "send called with $@"
+		return 1
+	fi
+
+	if [ "x$1" = "x" ] ; then
+		echo no file to send
+		return 1
+	fi
+
+	if [ ! -r "$1" ] ; then
+		echo "file $1 is not readable"
+		ls -l $1
+		return 1
+	fi
+
+	if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ] ; then
+		local branch=$TRAVIS_PULL_REQUEST_BRANCH
+	else
+		local branch=$TRAVIS_BRANCH
+	fi
+
+	# Temporarily disable tracing from here
+	set +x
+
+	local FROM=$1
+	local TO=${branch}_$2
+	local LATE=${branch}_latest_libiio${LDIST}$3
+	local GLOB=${DEPLOY_TO}/${branch}_libiio-*
+
+	echo attemting to deploy $FROM to $TO
+	echo and ${branch}_libiio${LDIST}$3
+	ssh -V
+
+	echo "cd ${DEPLOY_TO}" > script$3
+	if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${TO} ; then
+		echo "rm ${TO}" >> script$3
+	fi
+	echo "put ${FROM} ${TO}" >> script$3
+	echo "ls -l ${TO}" >> script$3
+	if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${LATE} ; then
+		echo "rm ${LATE}" >> script$3
+	fi
+	echo "symlink ${TO} ${LATE}" >> script$3
+	echo "ls -l ${LATE}" >> script$3
+	echo "bye" >> script$3
+
+	sftp ${EXTRA_SSH} -b script$3 ${SSHUSER}@${SSHHOST}
+
+	# limit things to a few files, so things don't grow forever
+	if [ "$3" = ".deb" ] ; then
+		for files in $(ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
+			"ls -lt ${GLOB}" | tail -n +100 | awk '{print $NF}')
+		do
+			ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
+				"rm ${DEPLOY_TO}/${files}"
+		done
+	fi
+
+	# Re-enable tracing
+	set -x
+}
