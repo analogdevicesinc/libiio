4d220b03d085553f3400da4a43242f5d4f18a8b2
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-11-22 17:05:22 +0200

    CI/travis: remove the `travis-ci` quirk
    Now that the inside_{bionic,centos}_docker.sh scripts have their LIBNAME
    parametrized, just cd to that folder in these scripts.
    All else should be just like in normal Travis-CI run from there, and the
    inside_docker scripts should be run inside Docker only.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/inside_bionic_docker.sh
+++ CI/travis/inside_bionic_docker.sh
@@ -1,24 +1,13 @@
 #!/bin/sh -xe
 
 LIBNAME="$1"
-TRAVIS_CI="$2"
 
-export TRAVIS_BUILD_DIR="/$LIBNAME"
+cd /$LIBNAME
 
 apt-get -qq update
 apt-get -y install sudo
 
 /$LIBNAME/CI/travis/before_install_linux default
-
-# Check we're in Travis-CI; the only place where this context is valid
-# It makes sure, users won't shoot themselves in the foot while running this script
-if [ "$TRAVIS_CI" == "travis-ci" ] ; then
-	mkdir -p /$LIBNAME/build
-	cd /$LIBNAME/build
-else
-	mkdir -p build
-	cd build
-fi
 
 /$LIBNAME/CI/travis/make_linux "$LIBNAME" default
 
