a369d1c0ec3194068e7283d198223426fb240c35
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-11-23 15:56:47 +0200

    Revert "CI/travis/lib.sh: add utility library & check deploy condition here"
    This reverts commit 3724ea604a7d584a2c6ab1381d2a81a420989f17.
    
    This patch exposes a security risk, by potentially displaying secret
    information in the logs.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/lib.sh
+++ CI/travis/lib.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -xe
 
-is_deployable_travis_ci_run() {
-	# Don't deploy on a Coverity build
-	[ -z "${COVERITY_SCAN_PROJECT_NAME}" ] || return 1
-
-	# If we don't have SSH keys, don't bother
-	[ -n "${ENCRYPTED_KEY}" ] || return 1
-	[ -n "${ENCRYPTED_IV}" ] || return 1
-}
-
 get_ldist() {
 	case "$(uname)" in
 	Linux*)
