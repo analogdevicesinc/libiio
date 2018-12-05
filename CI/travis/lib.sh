c5b72a45806a1563ac369924aa2ed74b792a13cc
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-12-05 12:02:18 +0200

    CI/travis/lib.sh: rename & indent send() function
    Rename send() -> upload_file_to_swdownloads().
    Since it's in lib.sh, it needs a better name to avoid potential name
    clashes with other local functions/code.
    
    Indent the code to make it a bit more readable.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/lib.sh
+++ CI/travis/lib.sh
@@ -138,62 +138,62 @@ brew_install_or_upgrade() {
 	done
 }
 
-send()
-{
-if [ "$#" -ne 3 ] ; then
-	echo "skipping deployment of something"
-        echo "send called with $@"
-	return
-fi
+upload_file_to_swdownloads() {
 
-if [ "x$1" = "x" ] ; then
-	echo no file to send
-	return
-fi
+	if [ "$#" -ne 3 ] ; then
+		echo "skipping deployment of something"
+		echo "send called with $@"
+		return
+	fi
 
-if [ ! -r "$1" ] ; then
-	echo "file $1 is not readable"
-	ls -l $1
-	return
-fi
+	if [ "x$1" = "x" ] ; then
+		echo no file to send
+		return
+	fi
 
-if [ $BRANCH_PULL ] ; then
-	branch=$BRANCH_PULL
-else
-	branch=$BRANCH
-fi
+	if [ ! -r "$1" ] ; then
+		echo "file $1 is not readable"
+		ls -l $1
+		return
+	fi
 
-FROM=$1
-TO=${branch}_$2
-LATE=${branch}_latest_libiio${LDIST}$3
-GLOB=${DEPLOY_TO}/${branch}_libiio-*
+	if [ $BRANCH_PULL ] ; then
+		branch=$BRANCH_PULL
+	else
+		branch=$BRANCH
+	fi
 
-echo attemting to deploy $FROM to $TO
-echo and ${branch}_libiio${LDIST}$3
-ssh -V
+	FROM=$1
+	TO=${branch}_$2
+	LATE=${branch}_latest_libiio${LDIST}$3
+	GLOB=${DEPLOY_TO}/${branch}_libiio-*
 
-echo "cd ${DEPLOY_TO}" > script$3
-if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${TO} ; then
-	echo "rm ${TO}" >> script$3
-fi
-echo "put ${FROM} ${TO}" >> script$3
-echo "ls -l ${TO}" >> script$3
-if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${LATE} ; then
-	echo "rm ${LATE}" >> script$3
-fi
-echo "symlink ${TO} ${LATE}" >> script$3
-echo "ls -l ${LATE}" >> script$3
-echo "bye" >> script$3
+	echo attemting to deploy $FROM to $TO
+	echo and ${branch}_libiio${LDIST}$3
+	ssh -V
 
-sftp ${EXTRA_SSH} -b script$3 ${SSHUSER}@${SSHHOST}
+	echo "cd ${DEPLOY_TO}" > script$3
+	if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${TO} ; then
+		echo "rm ${TO}" >> script$3
+	fi
+	echo "put ${FROM} ${TO}" >> script$3
+	echo "ls -l ${TO}" >> script$3
+	if curl -m 10 -s -I -f -o /dev/null http://swdownloads.analog.com/cse/travis_builds/${LATE} ; then
+		echo "rm ${LATE}" >> script$3
+	fi
+	echo "symlink ${TO} ${LATE}" >> script$3
+	echo "ls -l ${LATE}" >> script$3
+	echo "bye" >> script$3
 
-# limit things to a few files, so things don't grow forever
-if [ "$3" = ".deb" ] ; then
-	for files in $(ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
-		"ls -lt ${GLOB}" | tail -n +100 | awk '{print $NF}')
-	do
-		ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
-			"rm ${DEPLOY_TO}/${files}"
-	done
-fi
+	sftp ${EXTRA_SSH} -b script$3 ${SSHUSER}@${SSHHOST}
+
+	# limit things to a few files, so things don't grow forever
+	if [ "$3" = ".deb" ] ; then
+		for files in $(ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
+			"ls -lt ${GLOB}" | tail -n +100 | awk '{print $NF}')
+		do
+			ssh ${EXTRA_SSH} ${SSHUSER}@${SSHHOST} \
+				"rm ${DEPLOY_TO}/${files}"
+		done
+	fi
 }
