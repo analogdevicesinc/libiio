f1b9df3105d9c0d00f2619748124652fd12dd2f9
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-12-21 04:08:40 -0500

    CI/travis/inside_docker.sh: create a single docker wrapper
    Now that we don't need the CentOS 7 hack, we can unify both inside_docker
    scripts into a single one.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/inside_docker.sh
+++ CI/travis/inside_docker.sh
@@ -1,15 +1,13 @@
 #!/bin/sh -xe
 
 LIBNAME="$1"
+OS_TYPE="$2"
 
 cd /$LIBNAME
 
-apt-get -qq update
-apt-get -y install sudo
+/$LIBNAME/CI/travis/before_install_linux "$OS_TYPE"
 
-/$LIBNAME/CI/travis/before_install_linux default
-
-/$LIBNAME/CI/travis/make_linux "$LIBNAME" default
+/$LIBNAME/CI/travis/make_linux "$OS_TYPE"
 
 # need to find this out inside the container
 . /${LIBNAME}/CI/travis/lib.sh
