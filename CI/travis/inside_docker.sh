528345baf3ffa8b1178e4a6e3435b82633963857
Robin Getz <robin.getz@analog.com>
2020-04-15 15:48:12 -0400

    Codacy (shellcheck): Double quote to prevent globbing and word splitting.
    Many places in the CI infrastructure, we do things like:
    
    echo $1
    
    rather than:
    
    echo "$1"
    
    bash will actually:
      - Split the first argument by IFS (spaces, tabs and line feeds).
      - Expand each of them as if it was a glob.
      - Join all the resulting strings and filenames with spaces.
      - then finally print the result.
    
    Quoting variables prevents word splitting and glob expansion, and prevents
    scripts from breaking when input contains spaces.
    
    So do that.
    
    Signed-off-by: Robin Getz <robin.getz@analog.com>

--- CI/travis/inside_docker.sh
+++ CI/travis/inside_docker.sh
@@ -6,7 +6,7 @@ OS_TYPE="$2"
 export INSIDE_DOCKER="1"
 export TRAVIS_BUILD_DIR="/$LIBNAME"
 
-cd /$LIBNAME
+cd "/$LIBNAME"
 
 if [ -d "/$LIBNAME/CI" ] ; then
 	CI="/$LIBNAME/CI"
@@ -18,13 +18,13 @@ else
 fi
 
 if [ -f "/$LIBNAME/inside-travis-ci-docker-env" ] ; then
-	. /$LIBNAME/inside-travis-ci-docker-env
+	. "/$LIBNAME/inside-travis-ci-docker-env"
 fi
 
-$CI/travis/before_install_linux "$OS_TYPE"
+"$CI/travis/before_install_linux" "$OS_TYPE"
 
-$CI/travis/make_linux "$OS_TYPE"
+"$CI/travis/make_linux" "$OS_TYPE"
 
 # need to find this out inside the container
-. $CI/travis/lib.sh
-echo "$(get_ldist)" > /${LIBNAME}/build/.LDIST
+. "$CI/travis/lib.sh"
+echo "$(get_ldist)" > "/${LIBNAME}/build/.LDIST"
