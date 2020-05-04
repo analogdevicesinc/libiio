analogdevicesinc/libiio

commit 3f305ee3f0ada31d4d26ac97f349912869825cfe

replace strcpy and snprintf in remaining user in libiio
https://cwe.mitre.org/data/definitions/120.html defines
strcpy as potentially dangerous, and is included in Microsoft's
banned.h

snprintf has a iio_ special version that uses sprintf_s with Microsoft
Compilers. sprintf_s guarantees that the buffer will be null-terminated
unless the buffer size is zero (among other things). Linux snprintf does
this by default.

replace strncpy with iio_strlcpy
	taking advantage of mandatory null termination in iio_strlcpy

replace snprintf with iio_snprintf

Signed-off-by: Robin Getz <robin.getz@analog.com>

local.c
@@ -223,8 +223,7 @@ static int set_channel_name(struct iio_channel *chn)
 		name = malloc(prefix_len);
 		if (!name)
 			return -ENOMEM;
-		strncpy(name, attr0, prefix_len - 1);
-		name[prefix_len - 1] = '\0';
+		iio_strlcpy(name, attr0, prefix_len - 1);
 		IIO_DEBUG("Setting name of channel %s to %s\n", chn->id, name);
 		chn->name = name;
 

dns_sd.c
@@ -290,7 +290,7 @@ int dnssd_discover_host(char *addr_str, size_t addr_len, uint16_t *port)
 
 	if (ddata) {
 		*port = ddata->port;
-		strncpy(addr_str, ddata->addr_str, addr_len);
+		iio_strlcpy(addr_str, ddata->addr_str, addr_len);
 	}
 
 host_fail:

dns_sd_bonjour.c
@@ -144,9 +144,9 @@ static void __cfnet_browser_cb (
 	dd->port = port;
 	dd->hostname = strdup(hostname);
 	if (have_v4) {
-		strncpy(dd->addr_str, address_v4, sizeof(dd->addr_str));
+		iio_strlcpy(dd->addr_str, address_v4, sizeof(dd->addr_str));
 	} else if(have_v6) {
-		strncpy(dd->addr_str, address_v6, sizeof(dd->addr_str));
+		iio_strlcpy(dd->addr_str, address_v6, sizeof(dd->addr_str));
 	}
 
 	IIO_DEBUG("DNS SD: added %s (%s:%d)\n", hostname, dd->addr_str, port);

iiod/ops.c
@@ -1262,7 +1262,7 @@ ssize_t get_trigger(struct parser_pdata *pdata, struct iio_device *dev)
 		ret = strlen(trigger->name);
 		print_value(pdata, ret);
 
-		snprintf(buf, sizeof(buf), "%s\n", trigger->name);
+		iio_snprintf(buf, sizeof(buf), "%s\n", trigger->name);
 		ret = write_all(pdata, buf, ret + 1);
 	} else {
 		print_value(pdata, ret);

iiod/usbd.c
@@ -106,14 +106,14 @@ static int usb_open_pipe(struct usbd_pdata *pdata, unsigned int pipe_id)
 	 * before opening the endpoints again. */
 	thread_pool_stop_and_wait(pdata->pool[pipe_id]);
 
-	snprintf(buf, sizeof(buf), "%s/ep%u", pdata->ffs, pipe_id * 2 + 1);
+	iio_snprintf(buf, sizeof(buf), "%s/ep%u", pdata->ffs, pipe_id * 2 + 1);
 	cpdata->ep_out = open(buf, O_WRONLY);
 	if (cpdata->ep_out < 0) {
 		err = -errno;
 		goto err_free_cpdata;
 	}
 
-	snprintf(buf, sizeof(buf), "%s/ep%u", pdata->ffs, pipe_id * 2 + 2);
+	iio_snprintf(buf, sizeof(buf), "%s/ep%u", pdata->ffs, pipe_id * 2 + 2);
 	cpdata->ep_in = open(buf, O_RDONLY);
 	if (cpdata->ep_in < 0) {
 		err = -errno;
@@ -361,7 +361,7 @@ int start_usb_daemon(struct iio_context *ctx, const char *ffs,
 		goto err_free_pdata_pool;
 	}
 
-	snprintf(buf, sizeof(buf), "%s/ep0", ffs);
+	iio_snprintf(buf, sizeof(buf), "%s/ep0", ffs);
 
 	pdata->ep0_fd = open(buf, O_RDWR);
 	if (pdata->ep0_fd < 0) {

network.c
@@ -1424,7 +1424,7 @@ struct iio_context * network_create_context(const char *host)
 		inet_ntop(AF_INET, &in->sin_addr, description, INET_ADDRSTRLEN);
 #else
 		char *tmp = inet_ntoa(in->sin_addr);
-		strncpy(description, tmp, len);
+		iio_strlcpy(description, tmp, len);
 #endif
 	}
 

utilities.c
@@ -180,8 +180,7 @@ void iio_library_get_version(unsigned int *major,
 	if (minor)
 		*minor = LIBIIO_VERSION_MINOR;
 	if (git_tag) {
-		strncpy(git_tag, LIBIIO_VERSION_GIT, 8);
-		git_tag[7] = '\0';
+		iio_strlcpy(git_tag, LIBIIO_VERSION_GIT, 8);
 	}
 }
 

commit 4fcba7d75b0ded682ac1e7c46c3faeb03b7b3660

local.c: fix off by one introduced in 3f305ee
when moving to iio_strlcpy in local.c during:
3f305ee3f0ada31d4d26ac97f349912869825cfe
we were off by one, so fix that.

Tested over network from FMCOMMS2/Zed Board (running Master) to Windows
(running 0.19 release).

Signed-off-by: Robin Getz <robin.getz@analog.com>

local.c
@@ -223,7 +223,7 @@ static int set_channel_name(struct iio_channel *chn)
 		name = malloc(prefix_len);
 		if (!name)
 			return -ENOMEM;
-		iio_strlcpy(name, attr0, prefix_len - 1);
+		iio_strlcpy(name, attr0, prefix_len);
 		IIO_DEBUG("Setting name of channel %s to %s\n", chn->id, name);
 		chn->name = name;
 