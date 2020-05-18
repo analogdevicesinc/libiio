3a023175465e51e11a805d47095eb798f4cccbc6
Robin Getz <robin.getz@analog.com>
2020-05-18 15:52:30 -0400

    pylint: fix issues in iio_readdev.py and iio_writedev.py
    as pointed out by pylint:
    - Access to a protected member _length of a client class
       is bad form, and don't do it.
    - Comments at the top of a file should be in docstring
      format, so do that too
    
    Signed-off-by: Robin Getz <robin.getz@analog.com>

--- bindings/python/examples/iio_writedev.py
+++ bindings/python/examples/iio_writedev.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-#
-# Copyright (C) 2020 Analog Devices, Inc.
-# Author: Cristian Iacob <cristian.iacob@analog.com>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+Copyright (C) 2020 Analog Devices, Inc.
+Author: Cristian Iacob <cristian.iacob@analog.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
 
 import time
 import sys
@@ -196,15 +197,15 @@ class DataWriter:
         num_samples = self.arguments.num_samples
 
         while app_running:
-            bytes_to_read = self.buffer._length if num_samples == 0 \
-                else min(self.buffer._length, num_samples * self.device.sample_size)
+            bytes_to_read = len(self.buffer) if num_samples == 0 \
+                else min(len(self.buffer), num_samples * self.device.sample_size)
             write_len = bytes_to_read
             data = []
 
             while bytes_to_read > 0:
                 read_data = sys.stdin.buffer.read(bytes_to_read)
                 if len(read_data) == 0:
-                    exit(0)
+                    sys.exit(0)
                 bytes_to_read -= len(read_data)
                 data.extend(read_data)
 
