5e9e82dbc1b29700d009068fbba1562771e9d227
Autogenerated by Travis CI <robin.getz@analog.com>
2020-06-19 07:03:43 +0000

    Deploy autogenerated docs for libiio v0.23-gb814d31
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

---
+++ v0.23/csharp/classiio_1_1ScanContext.js
@@ -0,0 +1,6 @@
+var classiio_1_1ScanContext =
+[
+    [ "get_dns_sd_backend_contexts", "classiio_1_1ScanContext.html#a097cc76f6b3f5630c0e17db97884bc36", null ],
+    [ "get_local_backend_contexts", "classiio_1_1ScanContext.html#a3a7e6bbee822fc27f6917a8c20390f9b", null ],
+    [ "get_usb_backend_contexts", "classiio_1_1ScanContext.html#a7f05f475f3f8c6ed8a9bb6118b42d52b", null ]
+];
---
+++ v0.23/csharp/search/properties_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['type_142',['type',['../classiio_1_1Channel.html#a0a8e7a2aef01d07302fdc71975d274d2',1,'iio::Channel']]]
+];
