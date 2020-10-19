analogdevicesinc/libiio

commit 8979da9e09ee4c6b3286b1253f0df9a0af7a2275

iio.h: add IIO_MOD_O2 modifier to enum
This started appearing in the builds:
  https://travis-ci.org/github/analogdevicesinc/libiio/jobs/736083344#L1133

The upstream kernel header got an update.
This change syncs it to libiio.

Updated in all bindings as well.

Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

channel.c
@@ -100,6 +100,7 @@ static const char * const modifier_names[] = {
 	[IIO_MOD_CO2] = "co2",
 	[IIO_MOD_ETHANOL] = "ethanol",
 	[IIO_MOD_H2] = "h2",
+	[IIO_MOD_O2] = "h2",
 	[IIO_MOD_VOC] = "voc",
 	[IIO_MOD_PM1] = "pm1",
 	[IIO_MOD_PM2P5] = "pm2p5",

bindings/csharp/Channel.cs
@@ -116,7 +116,8 @@ namespace iio
             IIO_MOD_PM4,
             IIO_MOD_PM10,
             IIO_MOD_ETHANOL,
-            IIO_MOD_H2
+            IIO_MOD_H2,
+            IIO_MOD_O2
         }
 
         /// <summary><see cref="iio.Channel.ChannelType"/> class:

bindings/python/iio.py
@@ -173,6 +173,7 @@ class ChannelModifier(Enum):
     IIO_MOD_PM10 = 41
     IIO_MOD_ETHANOL = 42
     IIO_MOD_H2 = 43
+    IIO_MOD_O2 = 44
 
 
 class ChannelType(Enum):

iio.h
@@ -190,6 +190,7 @@ enum iio_modifier {
 	IIO_MOD_PM10,
 	IIO_MOD_ETHANOL,
 	IIO_MOD_H2,
+	IIO_MOD_O2,
 };
 
 /* ---------------------------------------------------------------------------*/

commit 0553bde9d02a70ae07b891aa79c7e2c68277eaa9

channel.c: fix typo for string
Was introduced via copy+paste error.

Fixes: 8979da9 ("iio.h: add IIO_MOD_O2 modifier to enum")
Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

channel.c
@@ -100,7 +100,7 @@ static const char * const modifier_names[] = {
 	[IIO_MOD_CO2] = "co2",
 	[IIO_MOD_ETHANOL] = "ethanol",
 	[IIO_MOD_H2] = "h2",
-	[IIO_MOD_O2] = "h2",
+	[IIO_MOD_O2] = "o2",
 	[IIO_MOD_VOC] = "voc",
 	[IIO_MOD_PM1] = "pm1",
 	[IIO_MOD_PM2P5] = "pm2p5",