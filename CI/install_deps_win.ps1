f02f21b342300fee862ca349357bc9d3e9d57836
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-12-15 14:36:26 +0200

    ci,azure-pipelines: add windows builds
    This change adds a minimal configuration to build some Windows artifacts
    for libiio.
    These are stored after the pipeline is done.
    
    Adapted from Travis Collins' work.
    Added into PowerShell scripts. Ideally we should not add any script-code
    into yaml files, as that makes it harder to scale, and even harder to
    migrate (if ever will be needed again).
    
    Publishing artifacts, the generated DLL, static library, and the iio.h
    header, which will be used in the next jobs.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ CI/install_deps_win.ps1
@@ -0,0 +1,23 @@
+
+git submodule update --init
+
+if (!(Test-Path deps)) {
+	mkdir deps
+}
+cd deps
+
+mkdir libxml
+wget https://www.zlatkovic.com/pub/libxml/64bit/libxml2-2.9.3-win32-x86_64.7z -OutFile "libxml.7z"
+7z x -y libxml.7z
+rm libxml.7z
+
+echo "Downloading deps..."
+cd C:\
+wget http://swdownloads.analog.com/cse/build/libiio-win-deps.zip -OutFile "libiio-win-deps.zip"
+7z x -y "C:\libiio-win-deps.zip"
+
+# Note: InnoSetup is already installed on Azure images; so don't run this step
+#       Running choco seems a bit slow; seems to save about 40-60 seconds here
+#choco install InnoSetup
+
+set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
