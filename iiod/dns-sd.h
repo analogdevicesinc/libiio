24538375f334c7003ee43610a4ca5d8418d40f5b
Paul Cercueil <paul@crapouillou.net>
2021-04-08 13:49:33 +0100

    iiod: Move Avahi support to its own dns-sd.c source file
    Avoid having a huge #ifdef block in iiod.c by moving this code to a
    dns-sd.c source file, that will be compiled conditionally.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ iiod/dns-sd.h
@@ -0,0 +1,17 @@
+/* SPDX-license-identifier: LGPL-v2-or-later */
+/*
+ * libiio - Library for interfacing industrial I/O (IIO) devices
+ *
+ * Copyright (C) 2021 Analog Devices, Inc.
+ * Author: Paul Cercueil <paul.cercueil@analog.com>
+ */
+
+#ifndef __IIOD_DNS_SD_H
+#define __IIOD_DNS_SD_H
+
+struct thread_pool;
+
+void start_avahi(struct thread_pool *pool);
+void stop_avahi(void);
+
+#endif /* __IIOD_DNS_SD_H */
