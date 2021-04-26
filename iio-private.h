ebf3c7e684bd43509a5787598e85e84c3e579509
Paul Cercueil <paul@crapouillou.net>
2021-04-26 15:35:11 +0100

    Use gnu_printf format with MinGW
    We don't actually *need* any of the GNU printf format specifiers.
    
    However, older versions of MinGW will complain about C99-compliant
    specifiers like %lld unless 'gnu_printf' is used.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iio-private.h
+++ iio-private.h
@@ -37,7 +37,9 @@
 #define iio_sscanf sscanf
 #endif
 
-#if defined(__GNUC__)
+#if defined(__MINGW32__)
+#   define __iio_printf __attribute__((__format__(gnu_printf, 3, 4)))
+#elif defined(__GNUC__)
 #   define __iio_printf __attribute__((__format__(printf, 3, 4)))
 #else
 #   define __iio_printf
