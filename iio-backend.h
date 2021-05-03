4c6876ba6bc7d20c11617ac56dee740989e3deb9
Paul Cercueil <paul@crapouillou.net>
2021-05-03 15:47:56 +0100

    iio-backend: Add create callback in backend ops structure
    Add a callback in the backend ops structure that can be used to create a
    IIO context from a URI string.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iio-backend.h
+++ iio-backend.h
@@ -66,6 +66,8 @@ enum iio_attr_type {
 };
 
 struct iio_backend_ops {
+	struct iio_context * (*create)(const struct iio_context_params *params,
+				       const char *uri);
 	struct iio_context * (*clone)(const struct iio_context *ctx);
 	ssize_t (*read)(const struct iio_device *dev, void *dst, size_t len,
 			uint32_t *mask, size_t words);
