ff96fc8279b689d0b6b2a2f9499899da9c73e6c1
Paul Cercueil <paul@crapouillou.net>
2021-05-06 11:29:33 +0100

    CI: Update build script for Windows
    Disable serial and USB backends.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- CI/build_win.ps1
+++ CI/build_win.ps1
@@ -7,5 +7,5 @@ $src_dir=$pwd
 mkdir build
 cd build
 
-cmake -G "$COMPILER" -A "$ARCH" -DPYTHON_BINDINGS=ON -DLIBXML2_LIBRARIES="$src_dir\deps\lib\libxml2.dll.a" ..
+cmake -G "$COMPILER" -A "$ARCH" -DENABLE_IPV6=OFF -DWITH_USB_BACKEND=OFF -DWITH_SERIAL_BACKEND=OFF -DPYTHON_BINDINGS=ON -DLIBXML2_LIBRARIES="$src_dir\deps\lib\libxml2.dll.a" ..
 cmake --build . --config Release
