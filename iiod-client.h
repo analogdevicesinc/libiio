5b72c044a5b12dc6162d2170a325de9a0aeef6bf
Paul Cercueil <paul@crapouillou.net>
2021-05-13 09:25:13 +0100

    iiod-client: Export symbols
    Exporting these symbols will allow the backends to be implemented as
    modules.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iiod-client.h
+++ iiod-client.h
@@ -11,6 +11,8 @@
 
 #include "iio-backend.h"
 
+#define __api __iio_api
+
 struct iiod_client;
 struct iiod_client_pdata;
 
@@ -26,78 +28,83 @@ struct iiod_client_ops {
 			     char *dst, size_t len);
 };
 
-void iiod_client_mutex_lock(struct iiod_client *client);
-void iiod_client_mutex_unlock(struct iiod_client *client);
+__api void iiod_client_mutex_lock(struct iiod_client *client);
+__api void iiod_client_mutex_unlock(struct iiod_client *client);
 
-struct iiod_client * iiod_client_new(const struct iio_context_params *params,
-				     struct iio_context_pdata *pdata,
-				     const struct iiod_client_ops *ops);
-void iiod_client_destroy(struct iiod_client *client);
+__api struct iiod_client *
+iiod_client_new(const struct iio_context_params *params,
+		struct iio_context_pdata *pdata,
+		const struct iiod_client_ops *ops);
 
-int iiod_client_get_version(struct iiod_client *client,
-			    struct iiod_client_pdata *desc,
-			    unsigned int *major, unsigned int *minor,
-			    char *git_tag);
+__api void iiod_client_destroy(struct iiod_client *client);
 
-int iiod_client_get_trigger(struct iiod_client *client,
-			    struct iiod_client_pdata *desc,
-			    const struct iio_device *dev,
-			    const struct iio_device **trigger);
+__api int iiod_client_get_version(struct iiod_client *client,
+				  struct iiod_client_pdata *desc,
+				  unsigned int *major, unsigned int *minor,
+				  char *git_tag);
 
-int iiod_client_set_trigger(struct iiod_client *client,
-			    struct iiod_client_pdata *desc,
-			    const struct iio_device *dev,
-			    const struct iio_device *trigger);
-
-int iiod_client_set_kernel_buffers_count(struct iiod_client *client,
-					 struct iiod_client_pdata *desc,
-					 const struct iio_device *dev,
-					 unsigned int nb_blocks);
-
-int iiod_client_set_timeout(struct iiod_client *client,
-			    struct iiod_client_pdata *desc,
-			    unsigned int timeout);
-
-ssize_t iiod_client_read_attr(struct iiod_client *client,
-			      struct iiod_client_pdata *desc,
-			      const struct iio_device *dev,
-			      const struct iio_channel *chn,
-			      const char *attr, char *dest, size_t len,
-			      enum iio_attr_type type);
-
-ssize_t iiod_client_write_attr(struct iiod_client *client,
-			       struct iiod_client_pdata *desc,
-			       const struct iio_device *dev,
-			       const struct iio_channel *chn,
-			       const char *attr, const char *src,
-			       size_t len, enum iio_attr_type type);
-
-int iiod_client_open_unlocked(struct iiod_client *client,
-			      struct iiod_client_pdata *desc,
-			      const struct iio_device *dev,
-			      size_t samples_count, bool cyclic);
-
-int iiod_client_close_unlocked(struct iiod_client *client,
-			       struct iiod_client_pdata *desc,
-			       const struct iio_device *dev);
-
-ssize_t iiod_client_read_unlocked(struct iiod_client *client,
+__api int iiod_client_get_trigger(struct iiod_client *client,
 				  struct iiod_client_pdata *desc,
 				  const struct iio_device *dev,
-				  void *dst, size_t len,
-				  uint32_t *mask, size_t words);
+				  const struct iio_device **trigger);
 
-ssize_t iiod_client_write_unlocked(struct iiod_client *client,
-				   struct iiod_client_pdata *desc,
-				   const struct iio_device *dev,
-				   const void *src, size_t len);
+__api int iiod_client_set_trigger(struct iiod_client *client,
+				  struct iiod_client_pdata *desc,
+				  const struct iio_device *dev,
+				  const struct iio_device *trigger);
 
-struct iio_context * iiod_client_create_context(struct iiod_client *client,
-						struct iiod_client_pdata *desc,
-						const struct iio_backend *backend,
-						const char *description,
-						const char **ctx_attrs,
-						const char **ctx_values,
-						unsigned int nb_ctx_attrs);
+__api int iiod_client_set_kernel_buffers_count(struct iiod_client *client,
+					       struct iiod_client_pdata *desc,
+					       const struct iio_device *dev,
+					       unsigned int nb_blocks);
+
+__api int iiod_client_set_timeout(struct iiod_client *client,
+				  struct iiod_client_pdata *desc,
+				  unsigned int timeout);
+
+__api ssize_t iiod_client_read_attr(struct iiod_client *client,
+				    struct iiod_client_pdata *desc,
+				    const struct iio_device *dev,
+				    const struct iio_channel *chn,
+				    const char *attr, char *dest, size_t len,
+				    enum iio_attr_type type);
+
+__api ssize_t iiod_client_write_attr(struct iiod_client *client,
+				     struct iiod_client_pdata *desc,
+				     const struct iio_device *dev,
+				     const struct iio_channel *chn,
+				     const char *attr, const char *src,
+				     size_t len, enum iio_attr_type type);
+
+__api int iiod_client_open_unlocked(struct iiod_client *client,
+				    struct iiod_client_pdata *desc,
+				    const struct iio_device *dev,
+				    size_t samples_count, bool cyclic);
+
+__api int iiod_client_close_unlocked(struct iiod_client *client,
+				     struct iiod_client_pdata *desc,
+				     const struct iio_device *dev);
+
+__api ssize_t iiod_client_read_unlocked(struct iiod_client *client,
+					struct iiod_client_pdata *desc,
+					const struct iio_device *dev,
+					void *dst, size_t len,
+					uint32_t *mask, size_t words);
+
+__api ssize_t iiod_client_write_unlocked(struct iiod_client *client,
+					 struct iiod_client_pdata *desc,
+					 const struct iio_device *dev,
+					 const void *src, size_t len);
+
+__api struct iio_context *
+iiod_client_create_context(struct iiod_client *client,
+			   struct iiod_client_pdata *desc,
+			   const struct iio_backend *backend,
+			   const char *description,
+			   const char **ctx_attrs,
+			   const char **ctx_values,
+			   unsigned int nb_ctx_attrs);
+
+#undef __api
 
 #endif /* _IIOD_CLIENT_H */
