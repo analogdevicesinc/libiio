449b24ca7794c50a5f470009799414e7cc531ab4
Paul Cercueil <paul@crapouillou.net>
2021-05-27 12:23:26 +0100

    Add new scan API
    I always found the scan API of libiio 0.x to be very bloated. Three
    different structures listed in the public API (and more internally) and
    *ten* API functions, with a lot of redundancy, is enough to get me very
    confused about how to properly use it. And if I'm getting confused, then
    a lot of people probably are too.
    
    Add a new scan API for libiio 1.x, meant to replace the 0.x ones.
    
    The new API has only one 'iio_scan' type, and 5 API functions -
    create/destroy the scan context, get number of results, get description,
    get URI. That's all you need.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- scan.c
+++ scan.c
@@ -19,6 +19,10 @@ struct iio_scan_context {
 	bool scan_local;
 };
 
+struct iio_scan {
+	struct iio_scan_result scan_result;
+};
+
 const char * iio_context_info_get_description(
 		const struct iio_context_info *info)
 {
@@ -192,4 +196,86 @@ void iio_scan_block_destroy(struct iio_scan_block *blk)
 	iio_context_info_list_free(blk->info);
 	iio_scan_context_destroy(blk->ctx);
 	free(blk);
+}
+
+static bool has_backend(const char *backends, const char *backend)
+{
+	return !backends || iio_list_has_elem(backends, backend);
+}
+
+struct iio_scan * iio_scan(const struct iio_context_params *params,
+			   const char *backends)
+{
+	struct iio_scan *ctx;
+	int ret;
+
+	if (!params)
+		params = get_default_params();
+
+	ctx = calloc(1, sizeof(*ctx));
+	if (!ctx) {
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	if (WITH_LOCAL_BACKEND && has_backend(backends, "local")) {
+		ret = local_context_scan(&ctx->scan_result);
+		if (ret < 0) {
+			prm_perror(params, -ret,
+				   "Unable to scan local context(s)");
+		}
+	}
+
+	if (WITH_USB_BACKEND && has_backend(backends, "usb")) {
+		ret = usb_context_scan(&ctx->scan_result);
+		if (ret < 0) {
+			prm_perror(params, -ret,
+				   "Unable to scan USB context(s)");
+		}
+	}
+
+	if (HAVE_DNS_SD && has_backend(backends, "ip")) {
+		ret = dnssd_context_scan(&ctx->scan_result);
+		if (ret < 0) {
+			prm_perror(params, -ret,
+				   "Unable to scan network context(s)");
+		}
+	}
+
+	return ctx;
+}
+
+void iio_scan_destroy(struct iio_scan *ctx)
+{
+	unsigned int i;
+
+	for (i = 0; i < ctx->scan_result.size; i++) {
+		free(ctx->scan_result.info[i]->description);
+		free(ctx->scan_result.info[i]->uri);
+		free(ctx->scan_result.info[i]);
+	}
+
+	free(ctx);
+}
+
+size_t iio_scan_get_results_count(const struct iio_scan *ctx)
+{
+	return ctx->scan_result.size;
+}
+
+const char *
+iio_scan_get_description(const struct iio_scan *ctx, size_t idx)
+{
+	if (idx >= ctx->scan_result.size)
+		return NULL;
+
+	return ctx->scan_result.info[idx]->description;
+}
+
+const char * iio_scan_get_uri(const struct iio_scan *ctx, size_t idx)
+{
+	if (idx >= ctx->scan_result.size)
+		return NULL;
+
+	return ctx->scan_result.info[idx]->uri;
 }
