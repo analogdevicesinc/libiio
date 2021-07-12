b3bfd6629eddd837dcfee893dbaacd4bc523f805
Paul Cercueil <paul@crapouillou.net>
2021-07-12 13:20:50 +0100

    xml: Read context version into context structure
    Store the context version that may appear in the XML string into the
    context structure.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- xml.c
+++ xml.c
@@ -407,10 +407,12 @@ static int iio_populate_xml_context_helper(struct iio_context *ctx, xmlNode *roo
 
 static struct iio_context * iio_create_xml_context_helper(xmlDoc *doc)
 {
-	const char *description = NULL;
+	const char *description = NULL, *git_tag = NULL, *content;
 	struct iio_context *ctx;
+	long major = 0, minor = 0;
 	xmlNode *root;
 	xmlAttr *attr;
+	char *end;
 	int err;
 
 	root = xmlDocGetRootElement(doc);
@@ -421,16 +423,41 @@ static struct iio_context * iio_create_xml_context_helper(xmlDoc *doc)
 	}
 
 	for (attr = root->properties; attr; attr = attr->next) {
-		if (!strcmp((char *) attr->name, "description"))
-			description = (const char *)attr->children->content;
-		else if (strcmp((char *) attr->name, "name"))
+		content = (const char *) attr->children->content;
+
+		if (!strcmp((char *) attr->name, "description")) {
+			description = content;
+		} else if (!strcmp((char *) attr->name, "version-major")) {
+			major = strtol(content, &end, 10);
+			if (*end != '\0')
+				IIO_WARNING("invalid format for major version\n");
+		} else if (!strcmp((char *) attr->name, "version-minor")) {
+			minor = strtol(content, &end, 10);
+			if (*end != '\0')
+				IIO_WARNING("invalid format for minor version\n");
+		} else if (!strcmp((char *) attr->name, "version-git")) {
+			git_tag = content;
+		} else if (strcmp((char *) attr->name, "name")) {
 			IIO_WARNING("Unknown parameter \'%s\' in <context>\n",
-					(char *) attr->children->content);
+				    content);
+		}
 	}
 
 	ctx = iio_context_create_from_backend(&xml_backend, description);
 	if (!ctx)
 		return NULL;
+
+	if (git_tag) {
+		ctx->major = major;
+		ctx->minor = minor;
+
+		ctx->git_tag = iio_strdup(git_tag);
+		if (!ctx->git_tag) {
+			iio_context_destroy(ctx);
+			errno = ENOMEM;
+			return NULL;
+		}
+	}
 
 	err = iio_populate_xml_context_helper(ctx, root);
 	if (err) {
