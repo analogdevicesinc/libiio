f96db2ad1d297700cfafc27be7c9be32556c2395
Raluca Chis <raluca.chis@analog.com>
2021-09-02 09:58:19 +0300

    CI:reconfigure Azure Pipelines artifacts
    - macOS: remove an extra line.
    - macOS: change the name of .pkg and .tar.gz files to be different for
    	 each macOS build.
    - macOS: publish .tar.gz file as an Azure Pipeline artifact.
    - linux: add ARTIFACTNAME to be availabla as an common env-var.
    - linux: set CPACK_HELP to FLAGS in order to have different name for
    	 artifacts in each linux build.
    - windows: separe comands in build_win.ps1 for win32 and x64 architecture
    - windows: generate .tar.gz file (using python 3.9).
    - windows: add publish_deps.ps1 script which collects all .dll dependencies
    	   to publish them as artifacts.
    - windows: add some tasks to azure_pipelines.yml in order to publish
    	   following files: iio.h header, .exe files, libiio-sharp.dll,
    	   .tar.gz files, .dll dependencies.
    
    Signed-off-by: Raluca Chis <raluca.chis@analog.com>

--- CI/build_win.ps1
+++ CI/build_win.ps1
@@ -4,8 +4,34 @@ $ARCH=$Env:ARCH
 
 $src_dir=$pwd
 
-mkdir build
-cd build
+if ($ARCH -eq "Win32") {
+	echo "Running cmake for $COMPILER on 32 bit..."
+	mkdir build-win32
+	cp .\libiio.iss.cmakein .\build-win32
+	cd build-win32
 
-cmake -G "$COMPILER" -A "$ARCH" -DENABLE_IPV6=OFF -DWITH_USB_BACKEND=OFF -DWITH_SERIAL_BACKEND=OFF -DPYTHON_BINDINGS=ON -DLIBXML2_LIBRARIES="$src_dir\deps\lib\libxml2.dll.a" ..
-cmake --build . --config Release
+	cmake -G "$COMPILER" -A "$ARCH" -DENABLE_IPV6=OFF -DWITH_USB_BACKEND=OFF -DWITH_SERIAL_BACKEND=OFF -DPYTHON_BINDINGS=ON -DCSHARP_BINDINGS:BOOL=ON -DLIBXML2_LIBRARIES="$src_dir\deps\lib\libxml2.dll.a" ..
+	cmake --build . --config Release
+	cp .\libiio.iss $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+
+	cd ../bindings/python
+	python.exe setup.py.cmakein sdist
+	Get-ChildItem dist\pylibiio-*.tar.gz | Rename-Item -NewName "libiio-py39-win32.tar.gz"
+	mv .\dist\*.gz .
+	rm .\dist\*.gz
+}else {
+        echo "Running cmake for $COMPILER on 64 bit..."
+        mkdir build-x64
+	cp .\libiio.iss.cmakein .\build-x64
+        cd build-x64
+
+        cmake -G "$COMPILER" -A "$ARCH" -DENABLE_IPV6=OFF -DWITH_USB_BACKEND=OFF -DWITH_SERIAL_BACKEND=OFF -DPYTHON_BINDINGS=ON -DCSHARP_BINDINGS:BOOL=ON -DLIBXML2_LIBRARIES="$src_dir\deps\lib\libxml2.dll.a" ..
+        cmake --build . --config Release
+	cp .\libiio.iss $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+
+	cd ../bindings/python
+        python.exe setup.py.cmakein sdist
+        Get-ChildItem dist\pylibiio-*.tar.gz | Rename-Item -NewName "libiio-py39-amd64.tar.gz"
+        mv .\dist\*.gz .
+        rm .\dist\*.gz
+}
