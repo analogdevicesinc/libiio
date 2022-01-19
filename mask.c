7ad2fa68d3e1a1c4066ecc5ab8774343f90d492f
Paul Cercueil <paul@crapouillou.net>
2022-01-19 13:44:09 +0000

    Introduce iio_channels_mask private struct
    This struct will be used later to store a bitmask of enabled channels.
    
    Add iio_channels_mask_set_bit(), iio_channels_mask_clear_bit(), and
    iio_channels_mask_test_bit() inline functions to manipulate this new
    object.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ mask.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ * libiio - Library for interfacing industrial I/O (IIO) devices
+ *
+ * Copyright (C) 2022 Analog Devices, Inc.
+ * Author: Paul Cercueil <paul.cercueil@analog.com>
+ */
+
+#include "iio-private.h"
+
+#include <errno.h>
+#include <string.h>
+
+struct iio_channels_mask * iio_create_channels_mask(unsigned int nb_channels)
+{
+	struct iio_channels_mask *mask;
+	size_t nb_words = (nb_channels + 31) / 32;
+
+	mask = zalloc(sizeof(*mask) + nb_words * sizeof(uint32_t));
+	if (mask)
+		mask->words = nb_words;
+
+	return mask;
+}
+
+int iio_channels_mask_copy(struct iio_channels_mask *dst,
+			   const struct iio_channels_mask *src)
+{
+	if (dst->words != src->words)
+		return -EINVAL;
+
+	memcpy(dst->mask, src->mask, src->words * sizeof(uint32_t));
+
+	return 0;
+}
