61a31c284f7b55d92a4c9515856d36d49938f940
Paul Cercueil <paul@crapouillou.net>
2022-02-15 13:20:00 +0000

    Add support for Tasks
    A iio_task is a worker thread, that is created with a work function.
    Client code can then enqueue objects into the task, that will be
    processed asynchronously by the iio_task through the work function.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- include/iio/iio-lock.h
+++ include/iio/iio-lock.h
@@ -15,6 +15,8 @@
 
 struct iio_mutex;
 struct iio_cond;
+struct iio_task;
+struct iio_task_token;
 struct iio_thrd;
 
 __api struct iio_mutex *iio_mutex_create(void);
@@ -33,6 +35,21 @@ __api void iio_cond_signal(struct iio_cond *cond);
 __api struct iio_thrd * iio_thrd_create(int (*thrd)(void *),
 					void *d, const char *name);
 __api int iio_thrd_join_and_destroy(struct iio_thrd *thrd);
+
+__api struct iio_task * iio_task_create(int (*task)(void *firstarg, void *d),
+					void *firstarg, const char *name);
+__api void iio_task_flush(struct iio_task *task);
+__api int iio_task_destroy(struct iio_task *task);
+
+__api void iio_task_start(struct iio_task *task);
+__api void iio_task_stop(struct iio_task *task);
+
+__api struct iio_task_token * iio_task_enqueue(struct iio_task *task, void *elm);
+__api int iio_task_enqueue_autoclear(struct iio_task *task, void *elm);
+
+__api _Bool iio_task_is_done(struct iio_task_token *token);
+__api int iio_task_sync(struct iio_task_token *token, unsigned int timeout_ms);
+__api void iio_task_cancel(struct iio_task_token *token);
 
 #undef __api
 
