eb7c4670dc347f4e547ba0e8146a44e3cdd3de9a
Robin Getz <robin.getz@analog.com>
2022-02-17 18:14:10 -0500

    Windows Builds: add $ErrorActionPreference = "Stop" to power shell scripts
    Windows Power Shell default is to ignore errors, and continue, so we
    miss things, if there were problems. Change this feature to exit on
    error.
    
    Also check the result of the build, and if it didn't work - throw an
    error
    
    Signed-off-by: Robin Getz <robin.getz@analog.com>

--- CI/generate_exe.ps1
+++ CI/generate_exe.ps1
@@ -1,3 +1,7 @@
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.2#erroractionpreference
+$ErrorActionPreference = "Stop"
+$ErrorView = "NormalView"
+
 SET PATH=packages\Tools.InnoSetup.5.6.1\tools
 iscc $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-16-2019-Win32\libiio.iss
 
