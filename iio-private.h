f8b7c613b94915abca2a0bc49b70173aa0e031d3
Paul Cercueil <paul@crapouillou.net>
2022-02-21 10:18:58 +0000

    dynamic: Remove iio_dynamic_scan
    The iio_dynamic_scan() function and all its dependencies are not needed
    anymore, and can be safely dropped.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iio-private.h
+++ iio-private.h
@@ -158,10 +158,6 @@ void iio_release_module(struct iio_module *module);
 
 const struct iio_backend * iio_module_get_backend(struct iio_module *module);
 
-struct iio_directory * iio_open_dir(const char *path);
-void iio_close_dir(struct iio_directory *dir);
-const char * iio_dir_get_next_file_name(struct iio_directory *dir);
-
 void free_channel(struct iio_channel *chn);
 void free_device(struct iio_device *dev);
 
