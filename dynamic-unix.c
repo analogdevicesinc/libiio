46f760f698fcb440b8f8a2a4082dd8e8d7a9e450
Paul Cercueil <paul@crapouillou.net>
2022-02-21 10:18:58 +0000

    dynamic: Remove iio_dynamic_scan
    The iio_dynamic_scan() function and all its dependencies are not needed
    anymore, and can be safely dropped.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- dynamic-unix.c
+++ dynamic-unix.c
@@ -7,15 +7,8 @@
  */
 
 #include "dynamic.h"
-#include "iio-private.h"
 
-#include <dirent.h>
 #include <dlfcn.h>
-#include <errno.h>
-
-struct iio_directory {
-	DIR *directory;
-};
 
 void * iio_dlopen(const char *path)
 {
@@ -30,38 +23,4 @@ void iio_dlclose(void *lib)
 const void * iio_dlsym(void *lib, const char *symbol)
 {
 	return dlsym(lib, symbol);
-}
-
-struct iio_directory * iio_open_dir(const char *path)
-{
-	struct iio_directory *entry;
-
-	entry = malloc(sizeof(*entry));
-	if (!entry)
-		return ERR_PTR(-ENOMEM);
-
-	entry->directory = opendir(path);
-	if (!entry->directory) {
-		free(entry);
-		return ERR_PTR(-errno);
-	}
-
-	return entry;
-}
-
-void iio_close_dir(struct iio_directory *dir)
-{
-	closedir(dir->directory);
-	free(dir);
-}
-
-const char * iio_dir_get_next_file_name(struct iio_directory *dir)
-{
-	struct dirent *dirent;
-
-	dirent = readdir(dir->directory);
-	if (!dirent)
-		return NULL;
-
-	return dirent->d_name;
 }
--- dynamic-windows.c
+++ dynamic-windows.c
@@ -7,16 +7,8 @@
  */
 
 #include "dynamic.h"
-#include "iio-backend.h"
-#include "iio-private.h"
 
-#include <errno.h>
 #include <windows.h>
-
-struct iio_directory {
-	char *lastpath;
-	HANDLE file;
-};
 
 void * iio_dlopen(const char *path)
 {
@@ -31,53 +23,4 @@ void iio_dlclose(void *lib)
 const void * iio_dlsym(void *lib, const char *symbol)
 {
 	return (const void *) GetProcAddress(lib, symbol);
-}
-
-struct iio_directory * iio_open_dir(const char *path)
-{
-	struct iio_directory *dir;
-	WIN32_FIND_DATA fdata;
-	char buf[PATH_MAX];
-
-	dir = zalloc(sizeof(*dir));
-	if (!dir)
-		return ERR_PTR(-ENOMEM);
-
-	iio_snprintf(buf, sizeof(buf), "%s\\*", path);
-
-	/* FindFirstFileA will always return the '.' folder if the directory
-	 * exists, so we don't care if the related fdata is lost. */
-	dir->file = FindFirstFileA(buf, &fdata);
-	if (dir->file == INVALID_HANDLE_VALUE) {
-		if (GetLastError() == ERROR_FILE_NOT_FOUND)
-			return ERR_PTR(-ENOENT);
-		else
-			return ERR_PTR(-EIO);
-	}
-
-	return dir;
-}
-
-void iio_close_dir(struct iio_directory *dir)
-{
-	if (dir->file)
-		FindClose(dir->file);
-	free (dir->lastpath);
-	free(dir);
-}
-
-const char * iio_dir_get_next_file_name(struct iio_directory *dir)
-{
-	WIN32_FIND_DATA fdata;
-
-	free(dir->lastpath);
-
-	do {
-		if (!FindNextFileA(dir->file, &fdata))
-			return NULL;
-	} while (fdata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
-
-	dir->lastpath = _strdup(fdata.cFileName);
-
-	return dir->lastpath;
 }
