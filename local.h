a7c224ad055c859c95326d48d5a66790f9d44c73
Paul Cercueil <paul@crapouillou.net>
2022-05-09 14:00:56 +0100

    local: Add support for MMAP interface with the new iio_block API
    The old MMAP interface is not well suited to the new block-based API. It
    requires some gymnastics for the blocks allocation and deletion, and for
    keeping track of the dequeued blocks; but it is still possible to
    support it.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- local.h
+++ local.h
@@ -10,17 +10,21 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
+struct iio_buffer_impl_pdata;
 struct iio_block_impl_pdata;
 struct iio_device;
 struct timespec;
 
 struct iio_buffer_pdata {
 	const struct iio_device *dev;
+	struct iio_buffer_impl_pdata *pdata;
 	int fd, cancel_fd;
 	unsigned int idx;
 	bool multi_buffer;
 	bool dmabuf_supported;
+	bool mmap_supported;
 	size_t size;
 };
 
@@ -43,5 +47,16 @@ void local_free_dmabuf(struct iio_block_pdata *pdata);
 int local_enqueue_dmabuf(struct iio_block_pdata *pdata,
 			 size_t bytes_used, bool cyclic);
 int local_dequeue_dmabuf(struct iio_block_pdata *pdata, bool nonblock);
+
+struct iio_block_pdata *
+local_create_mmap_block(struct iio_buffer_pdata *pdata,
+			size_t size, void **data);
+void local_free_mmap_block(struct iio_block_pdata *pdata);
+
+int local_enqueue_mmap_block(struct iio_block_pdata *pdata,
+			     size_t bytes_used, bool cyclic);
+int local_dequeue_mmap_block(struct iio_block_pdata *pdata, bool nonblock);
+
+struct iio_buffer_impl_pdata * local_alloc_mmap_buffer_impl(void);
 
 #endif /* __IIO_LOCAL_H */
