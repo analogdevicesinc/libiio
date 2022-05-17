0d812848594383512c89f6e08031056cd6d52c83
Paul Cercueil <paul@crapouillou.net>
2022-05-17 11:22:17 +0100

    examples: Use SPDX license identifier
    Use a SPDX license identifier instead of a wall of text.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- examples/ad9371-iiostream.c
+++ examples/ad9371-iiostream.c
@@ -1,23 +1,10 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
 /*
  * libiio - AD9371 IIO streaming example
  *
  * Copyright (C) 2014 IABG mbH
  * Author: Michael Feilen <feilen_at_iabg.de>
  * Copyright (C) 2017 Analog Devices Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
 
 #include <stdbool.h>
