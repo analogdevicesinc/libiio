ac9859a66bf7594313ba711ead0980b94845683f
Paul Cercueil <paul@crapouillou.net>
2022-11-30 11:21:32 +0000

    local: Implement .disable_cpu_access() callback
    Implement the .disable_cpu_access() callback. This will only work when
    the DMABUF interface is used.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- local.c
+++ local.c
@@ -1697,6 +1697,18 @@ static int local_read_event(struct iio_event_stream_pdata *pdata,
 	return 0;
 }
 
+static int local_disable_cpu_access(struct iio_block_pdata *pdata, bool disable)
+{
+	if (WITH_LOCAL_DMABUF_API && pdata->buf->dmabuf_supported) {
+		if (pdata->cpu_access_disabled == disable)
+			return 0;
+
+		return local_dmabuf_disable_cpu_access(pdata, disable);
+	}
+
+	return -EINVAL;
+}
+
 static const struct iio_backend_ops local_ops = {
 	.scan = local_context_scan,
 	.create = local_create_context,
@@ -1722,6 +1734,8 @@ static const struct iio_backend_ops local_ops = {
 	.open_ev = local_open_events_fd,
 	.close_ev = local_close_events_fd,
 	.read_ev = local_read_event,
+
+	.disable_cpu_access = local_disable_cpu_access,
 };
 
 const struct iio_backend iio_local_backend = {
