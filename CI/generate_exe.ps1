f51030aca11eee61a341cec14a9ea712e342c9f7
Raluca Groza <raluca.groza@analog.com>
2023-05-02 15:13:34 +0300

    CI:add new builds
    Signed-off-by: Raluca Groza <raluca.groza@analog.com>

--- CI/generate_exe.ps1
+++ CI/generate_exe.ps1
@@ -2,6 +2,8 @@
 $ErrorActionPreference = "Stop"
 $ErrorView = "NormalView"
 
+dir D:\a\1\a\Windows-VS-2019-x64\
+
 iscc $env:BUILD_ARTIFACTSTAGINGDIRECTORY\Windows-VS-2019-x64\libiio.iss
 
 Get-ChildItem $env:BUILD_ARTIFACTSTAGINGDIRECTORY -Force -Recurse | Remove-Item -Force -Recurse
