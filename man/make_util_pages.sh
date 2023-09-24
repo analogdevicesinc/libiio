f292e21a136b556ab8cb26ac188432551454791e
David Runge <dave@sleepmap.de>
2023-09-24 18:59:50 +0200

    Rename utility files so they are not installed as man pages
    Rename the utility files to not have a .1 file ending, so that they
    are not installed to the chapter one man page directory, when users are
    doing an in-tree build.
    
    Signed-off-by: David Runge <dave@sleepmap.de>

--- man/make_util_pages.sh
+++ man/make_util_pages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dir=$1
-if [ -z "${dir}" -o ! -f "${dir}/iio_common_cmds.1" -o ! -f "${dir}/iio_common_opts.1" -o ! -f "${dir}/iio_common_footer.1" ] ; then
+if [ -z "${dir}" -o ! -f "${dir}/iio_common_cmds.var" -o ! -f "${dir}/iio_common_opts.var" -o ! -f "${dir}/iio_common_footer.var" ] ; then
 	echo "not pointing to src directory ${dir}" 1>&2
 	exit
 fi
@@ -14,8 +14,8 @@ fi
 app=$(grep "^.TH" "${file}" | head -1 | awk '{print $2}')
 
 sed '/##COMMON_COMMANDS_START##/Q' "${file}"
-sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_cmds.1"
+sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_cmds.var"
 sed -n '/^##COMMON_COMMANDS_STOP##/,${p;/^##COMMON_OPTION_START##/q}' "${file}" |  grep -v "##COMMON_"
-sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_opts.1"
+sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_opts.var"
 sed -ne '/^##COMMON_OPTION_STOP#/,$ p' "${file}" | grep -v "##COMMON_OPTION_STOP##"
-sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_footer.1"
+sed "s/##APP_NAME##/${app}/g" "${dir}/iio_common_footer.var"
