e2fb9798fefa27d2f9896fe1ea8b22f7370f4885
Paul Cercueil <paul@crapouillou.net>
2023-09-29 10:14:00 +0200

    serial: Update serial_get_trigger() callback
    Update serial_get_trigger() to match the prototype defined in the
    backends API.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- serial.c
+++ serial.c
@@ -201,13 +201,13 @@ static void serial_shutdown(struct iio_context *ctx)
 	sp_free_port(ctx_pdata->port);
 }
 
-static int serial_get_trigger(const struct iio_device *dev,
-			      const struct iio_device **trigger)
+static const struct iio_device *
+serial_get_trigger(const struct iio_device *dev)
 {
 	const struct iio_context *ctx = iio_device_get_context(dev);
 	struct iio_context_pdata *pdata = iio_context_get_pdata(ctx);
 
-	return iiod_client_get_trigger(pdata->iiod_client, dev, trigger);
+	return iiod_client_get_trigger(pdata->iiod_client, dev);
 }
 
 static int serial_set_trigger(const struct iio_device *dev,
