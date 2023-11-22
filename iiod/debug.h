d7772bbe4e9ac62321a75e66bf8b7b52b781a6b7
Paul Cercueil <paul@crapouillou.net>
2023-11-22 16:51:41 +0100

    iiod: Switch to use the new Libiio debug macros
    Some error messages were converted to use IIO_PERROR() so that they also
    print the error message and code.
    
    This opens the door to have --quiet and --verbose (or --debug) options
    to IIOD, since Libiio allows to control the verbosity at runtime.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- iiod/debug.h
+++ iiod/debug.h
@@ -9,47 +9,23 @@
 #ifndef __IIOD_DEBUG_H__
 #define __IIOD_DEBUG_H__
 
-#include "../iio-config.h"
+#include <iio/iio-debug.h>
 
-#include <stdio.h>
+extern struct iio_context_params iiod_params;
 
-#define NoLog_L 0
-#define Error_L 1
-#define Warning_L 2
-#define Info_L 3
-#define Debug_L 4
-
-/* -------------------- */
-
-/* Many of these debug printf include a Flawfinder: ignore, this is because,
- * according to https://cwe.mitre.org/data/definitions/134.html which describes
- * functions that accepts a format string as an argument, but the format
- * string originates from an external source. All the IIO_DEBUG, IIO_INFO,
- * IIO_WARNING, and IIO_ERRRO functions are called internally from the
- * library, have fixed format strings and can not be modified externally.
- */
 #define IIO_DEBUG(...) \
-    do { \
-	if (LOG_LEVEL >= Debug_L) \
-            fprintf(stdout, "DEBUG: " __VA_ARGS__); /* Flawfinder: ignore */ \
-    } while (0)
+	prm_dbg(&iiod_params, __VA_ARGS__)
 
 #define IIO_INFO(...) \
-    do { \
-	if (LOG_LEVEL >= Info_L) \
-            fprintf(stdout, __VA_ARGS__); /* Flawfinder: ignore */ \
-    } while (0)
+	prm_info(&iiod_params, __VA_ARGS__)
 
 #define IIO_WARNING(...) \
-    do { \
-	if (LOG_LEVEL >= Warning_L) \
-            fprintf(stderr, "WARNING: " __VA_ARGS__); /* Flawfinder: ignore */ \
-    } while (0)
+	prm_warn(&iiod_params, __VA_ARGS__)
 
 #define IIO_ERROR(...) \
-    do { \
-	if (LOG_LEVEL >= Error_L) \
-            fprintf(stderr, "ERROR: " __VA_ARGS__); /* Flawfinder: ignore */ \
-    } while (0)
+	prm_err(&iiod_params, __VA_ARGS__)
+
+#define IIO_PERROR(err, ...) \
+	prm_perror(&iiod_params, err, __VA_ARGS__)
 
 #endif /* __IIOD_DEBUG_H__ */
