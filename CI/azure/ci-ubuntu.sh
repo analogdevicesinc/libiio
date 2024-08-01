9fd8f5754e40d95be795248523e23ece693d5ead
Andreea Andrisan <Andreea.Andrisan@analog.com>
2024-08-01 11:28:43 +0300

    ci:azure:ci-ubuntu.sh: configure /ci directory as safe
    In order to extract the git sha of the last commit from libiio the
    directory where a volume of the project was created needs to be set as
    safe, otherwise an error message will appear.
    
    Signed-off-by: Andreea Andrisan <Andreea.Andrisan@analog.com>

--- CI/azure/ci-ubuntu.sh
+++ CI/azure/ci-ubuntu.sh
@@ -2,6 +2,7 @@
 set -x
 uname -a
 echo "$PWD"
+git config --global --add safe.directory /ci
 mkdir build && cd build
 cmake .. -Werror=dev -DCOMPILE_WARNING_AS_ERROR=ON -DWITH_SERIAL_BACKEND=ON -DWITH_EXAMPLES=ON -DPYTHON_BINDINGS=ON -DCPP_BINDINGS=ON -DENABLE_PACKAGING=ON -DCPACK_SYSTEM_NAME="${ARTIFACTNAME}"
 make
