7af64cf18ab2a77e602c7182b06cb9009065687d
Andreea Andrisan <Andreea.Andrisan@analog.com>
2024-09-12 13:33:38 +0300

    CI: integrate dependencies build script for msvc
    Switch to new script and modify the paths form cmake command and also
    the path from where the dll files were copied.
    
    The old script build_win.ps1 was renamed in build_win_msvc.ps1 because
    now it supports just the msvc compiler, eliminating the support for
    mingw because was not needed anymore.
    
    Signed-off-by: Andreea Andrisan <Andreea.Andrisan@analog.com>

--- CI/publish_deps.ps1
+++ CI/publish_deps.ps1
@@ -2,20 +2,7 @@
 $ErrorActionPreference = "Stop"
 $ErrorView = "NormalView"
 
-$src_dir=$pwd
-$COMPILER=$Env:COMPILER
-
-cd $src_dir
-mkdir dependencies
-cd dependencies
-wget http://swdownloads.analog.com/cse/build/libiio-deps-20220517.zip -OutFile "libiio-win-deps.zip"
-7z x -y "libiio-win-deps.zip"
-
-# Version numbers inside this directory change all the time; print what's
-# currently in the folder to make it easier to debug CI breakages on MinGW.
-dir C:\ghcup\ghc\
-
-if ($COMPILER -eq "MinGW Makefiles") {
+if ("$Env:COMPILER" -eq "MinGW Makefiles") {
 	cp C:\msys64\mingw64\bin\libserialport-0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libusb-1.0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libxml2-2.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
@@ -27,18 +14,19 @@ if ($COMPILER -eq "MinGW Makefiles") {
 	cp C:\msys64\mingw64\bin\libwinpthread-1.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libstdc++-6.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 } else {
-	cp $src_dir\dependencies\libs\64\libxml2.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-	cp $src_dir\dependencies\libs\64\libserialport-0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-	cp $src_dir\dependencies\libs\64\libusb-1.0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-	cp $src_dir\dependencies\libs\64\libzstd.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-
-	if ($COMPILER -eq "Visual Studio 16 2019") {
-		cd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.29.30133\x64\Microsoft.VC142.CRT'
-		cp .\msvcp140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-		cp .\vcruntime140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-	} else {
+	if ( "$Env:COMPILER" -eq "Visual Studio 17 2022" ){
+		$VS_version="VS2022"
 		cd 'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Redist\MSVC\14.29.30133\x64\Microsoft.VC142.CRT'
 		cp .\msvcp140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 		cp .\vcruntime140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+	}elseif ( "$Env:COMPILER" -eq "Visual Studio 16 2019" ) {
+		$VS_version="VS2019"
+		cd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.29.30133\x64\Microsoft.VC142.CRT'
+		cp .\msvcp140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+		cp .\vcruntime140.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	}
+	cp $Env:BUILD_SOURCESDIRECTORY\deps\libxml2-install\bin\libxml2.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+	cp $Env:BUILD_SOURCESDIRECTORY\deps\libusb\$VS_version\MS64\dll\libusb-1.0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+	cp $Env:BUILD_SOURCESDIRECTORY\deps\libserialport\x64\Release\libserialport.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+	cp $Env:BUILD_SOURCESDIRECTORY\deps\zstd\build\VS2010\bin\x64_Release\libzstd.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 }
