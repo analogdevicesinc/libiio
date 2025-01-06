829d390027666ce037794e517cb3e4f2dd86db2c
Pagadarai <Srikanth.Pagadarai@analog.com>
2025-01-06 16:31:45 -0600

    matlab bindings: [WIP] Added tests

---
+++ bindings/matlab/codegen/genlibiioAPI.m
@@ -0,0 +1,24 @@
+tic;
+functionNames = {...
+    'iioReadDevAttrLonglong.m',...
+    'iioWriteDevAttrLonglong.m',...
+    };
+cfg = coder.config('dll');
+cfg.TargetLang = 'C';
+cfg.FilePartitionMethod = 'SingleFile';
+cfg.RuntimeChecks = true;
+cfg.GenCodeOnly = true;
+cfg.EnableAutoExtrinsicCalls = false;
+cfg.MATLABSourceComments = true;
+% cfg.GenerateReport = true;
+% cfg.LaunchReport = true;
+cfg.HighlightPotentialDataTypeIssues = true;
+
+outputLIBName = 'mlibiio';
+cfg.HardwareImplementation.TargetHWDeviceType = 'Intel->x86-64 (Linux 64)'; %'Generic->32-bit Embedded Processor';
+result = codegen('-config','cfg',functionNames{:},'-O ','disable:openmp','-o',outputLIBName);
+% result = codegen('-config','cfg',functionNames{:},...
+% '-args',coder.opaque('const struct iio_context_params *'),...
+% '-O ','disable:openmp','-o',outputLIBName);
+assert(result.summary.passed)
+toc;
---
+++ bindings/matlab/codegen/iioReadDevAttrLonglong.m
@@ -0,0 +1,28 @@
+function [status, attrName, value] = iioReadDevAttrLonglong(uri, phyDevName, devAttrName)
+    assert(isa(uri,'char') && all(size(uri) <= [1,20]));
+    assert(isa(phyDevName,'char') && all(size(phyDevName) <= [1,20]));
+    assert(isa(devAttrName,'char') && all(size(devAttrName) <= [1,20]));
+
+    % Get Context
+    iioCtxPtr = adi.libiio.context.iio_create_context(uri);
+    status = -int32(iioCtxPtr==coder.opaque('struct iio_context*', 'NULL'));
+    if status ~= 0
+        value = int64(0);
+        return;
+    end
+
+    % Get PhyDev Pointer
+    iioPhyDevPtr = adi.libiio.context.iio_context_find_device(iioCtxPtr, phyDevName);
+
+    % Get Attribute Pointer
+    iioPhyDevAttrPtr = adi.libiio.device.iio_device_find_attr(iioPhyDevPtr, devAttrName);
+
+    % get Attribute Name
+    attrName = adi.libiio.attribute.iio_attr_get_name(iioPhyDevAttrPtr);
+
+    % Read Attribute
+    [status, value] = adi.libiio.attribute.iio_attr_read_longlong(iioPhyDevAttrPtr);
+
+    % Destroy Context
+    adi.libiio.context.iio_context_destroy(iioCtxPtr);
+end
