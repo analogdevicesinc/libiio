407555841a583bfbf9be07305c0ccbeefe95523b
Dan Nechita <dan.nechita@analog.com>
2025-01-10 15:47:13 +0200

    iio.h: Add new iio_chan_type entry ('attention') Add a new iio_chan_type enum entry that was added in kernel v6.13-rc6.
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- channel.c
+++ channel.c
@@ -53,6 +53,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_DELTA_VELOCITY] = "delta_velocity",
 	[IIO_COLORTEMP] = "colortemp",
 	[IIO_CHROMATICITY] = "chromaticity",
+	[IIO_ATTENTION] = "attention",
 };
 
 static const char * const hwmon_chan_type_name_spec[] = {
