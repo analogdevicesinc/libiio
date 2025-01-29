dab52a0664a624c64f0fadbeb309fdea4ab62f35
Pagadarai <Srikanth.Pagadarai@analog.com>
2025-01-29 16:27:43 -0600

    matlab bindings: Added test to check that library is loaded

---
+++ bindings/matlab/test/testLoadLibrary.m
@@ -0,0 +1,10 @@
+if isunix
+    hfile = '/usr/share/libiio/matlab/iio-wrapper.h';
+    loadlibraryArgs = {hfile,'includepath','/usr/local/include','addheader','iio.h'};
+    [a2, b2] = loadlibrary('libiio', loadlibraryArgs{:});
+    libfunctions('libiio')
+elseif ispc
+    % Code to run on Windows platform
+end
+    
+    
