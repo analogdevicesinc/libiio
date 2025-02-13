8b9fde059974bd800f0b4d65ef5ca26426c0bcf6
Dan Nechita <dan.nechita@analog.com>
2025-02-13 19:49:27 +0200

    csharp: Ensure iio scan blocks get destroyed after use
    Avoid keeping a pointer to a scan block that was never destroyed, either
    at the end of the ScanContext object's lifecycle or when calling any of
    the 'get_dns_sd_backend_contexts', 'get_usb_backend_contexts', or
    'get_local_backend_contexts' functions. These functions replace the
    pointer with a new one, leaving the old pointer undestroyed.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- bindings/csharp/ScanContext.cs
+++ bindings/csharp/ScanContext.cs
@@ -17,7 +17,6 @@ namespace iio
     /// Class for getting information about the available contexts.</summary>
     public class ScanContext
     {
-        private IntPtr scan_block;
 
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern IntPtr iio_create_scan_block([In] string backend, uint flags);
@@ -34,14 +33,20 @@ namespace iio
         [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern long iio_scan_block_scan(IntPtr blk);
 
+        [DllImport("libiio.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void iio_scan_block_destroy(IntPtr blk);
+
         /// <summary>
         /// Gets the uri and the description of all available contexts with USB backend. 
         /// </summary>
         /// <returns>a <see cref="Dictionary{String, String}"/> containing the context's uri as key and its description as value.</returns>
         public Dictionary<string, string> get_usb_backend_contexts()
         {
-            this.scan_block = iio_create_scan_block("usb", 0);
-            return get_contexts_info();
+            IntPtr scan_block = iio_create_scan_block("usb", 0);
+            Dictionary<string, string> context_info_dict = get_contexts_info(scan_block);
+            iio_scan_block_destroy(scan_block);
+
+            return context_info_dict;
         }
 
         // <summary>
@@ -50,8 +55,11 @@ namespace iio
         /// <returns>a <see cref="Dictionary{String, String}"/> containing the context's uri as key and its description as value.</returns>
         public Dictionary<string, string> get_local_backend_contexts()
         {
-            this.scan_block = iio_create_scan_block("local", 0);
-            return get_contexts_info();
+            IntPtr scan_block = iio_create_scan_block("local", 0);
+            Dictionary<string, string> context_info_dict = get_contexts_info(scan_block);
+            iio_scan_block_destroy(scan_block);
+
+            return context_info_dict;
         }
 
         // <summary>
@@ -60,18 +68,21 @@ namespace iio
         /// <returns>a <see cref="Dictionary{String, String}"/> containing the context's uri as key and its description as value.</returns>
         public Dictionary<string, string> get_dns_sd_backend_contexts()
         {
-            this.scan_block = iio_create_scan_block("ip", 0);
-            return get_contexts_info();
+            IntPtr scan_block = iio_create_scan_block("ip", 0);
+            Dictionary<string, string> context_info_dict = get_contexts_info(scan_block);
+            iio_scan_block_destroy(scan_block);
+
+            return context_info_dict;
         }
 
-        private Dictionary<string, string> get_contexts_info()
+        private Dictionary<string, string> get_contexts_info(IntPtr scan_block)
         {
-            uint contexts_count = (uint)iio_scan_block_scan(this.scan_block);
+            uint contexts_count = (uint)iio_scan_block_scan(scan_block);
             Dictionary<string, string> contexts_info = new Dictionary<string, string>();
 
             for (uint i = 0; i < contexts_count; i++)
             {
-                IntPtr info = iio_scan_block_get_info(this.scan_block, i);
+                IntPtr info = iio_scan_block_get_info(scan_block, i);
                 string uri = Marshal.PtrToStringAnsi(iio_context_info_get_uri(info));
                 string description = Marshal.PtrToStringAnsi(iio_context_info_get_description(info));
                 contexts_info[uri] = description;
