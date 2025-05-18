c99e6493d2aa9dbd40d2d0e24f0bacdfab0fe6c0
Robin Getz <rgetz503@gmail.com>
2025-05-17 20:47:40 -0400

    mingw64 - fix CI issues
     - update path / binary name
     - install python-setuptools
    
    Signed-off-by: Robin Getz <rgetz503@gmail.com>

--- CI/azure/build_mingw.sh
+++ CI/azure/build_mingw.sh
@@ -2,12 +2,17 @@
 set -xe
 
 init_env() {
+    # Define architecture and prefix
     export ARCH=x86_64
     export MINGW_VERSION=mingw64
-    export CC=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-gcc.exe
-    export CXX=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-g++.exe
-    export CMAKE="/$MINGW_VERSION/bin/cmake"
 
+    # Set up compilers and tools using absolute Windows paths
+    export CC=/c/msys64/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-gcc.exe
+    export CXX=/c/msys64/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-g++.exe
+    export CMAKE=/c/msys64/${MINGW_VERSION}/bin/cmake.exe
+
+    # Prepend to PATH so CMake and compiler are discoverable
+    export PATH="/c/msys64/${MINGW_VERSION}/bin:$PATH"
 }
 
 install_pacman_deps() {
@@ -16,11 +21,16 @@ install_pacman_deps() {
     mingw-w64-x86_64-zstd \
     mingw-w64-x86_64-libxml2 \
     mingw-w64-x86_64-python3 \
+    mingw-w64-x86_64-python-pip \
+    mingw-w64-x86_64-python-setuptools \
     mingw-w64-x86_64-cmake \
     mingw-w64-x86_64-gcc \
     cmake
     "
     echo "$WINDEPS" | xargs pacman -S --noconfirm --needed
+
+    ls -l "${CC}"
+    $CC --version
 }
 
 build_libiio() {
