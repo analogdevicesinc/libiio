c17c40719fe2634a5d710eeb61db29c1864dafd9
Andreea Andrisan <Andreea.Andrisan@analog.com>
2025-06-02 12:57:06 +0300

    azure-pipelines.yml: add support for ubuntu-24.04
    Include ubuntu-24.04 on the build pipeline
    Fixes https://github.com/analogdevicesinc/libiio/issues/1292
    
    Signed-off-by: Andreea Andrisan <Andreea.Andrisan@analog.com>

--- CI/azure/prepare_assets.sh
+++ CI/azure/prepare_assets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 release_artifacts() {
-        local deb_linux_assets='Fedora-34 Fedora-28 Ubuntu-20.04 Ubuntu-22.04 Debian-11 Debian-12 openSUSE-15.4 CentOS-7'
+        local deb_linux_assets='Fedora-34 Fedora-28 Ubuntu-20.04 Ubuntu-22.04 Ubuntu-24.04 Debian-11 Debian-12 openSUSE-15.4 CentOS-7'
         cd "${BUILD_ARTIFACTSTAGINGDIRECTORY}"
         for i in $deb_linux_assets; do
                 cd "Linux-${i}"
@@ -71,7 +71,7 @@ release_artifacts() {
 }
 
 swdownloads_artifacts() {
-        local linux_dist='Fedora-34 Fedora-28 Ubuntu-20.04 Ubuntu-22.04 Debian-11 Debian-12 openSUSE-15.4 CentOS-7'
+        local linux_dist='Fedora-34 Fedora-28 Ubuntu-20.04 Ubuntu-22.04 Ubuntu-24.04 Debian-11 Debian-12 openSUSE-15.4 CentOS-7'
         for distribution in $linux_dist; do
 		cd "${BUILD_ARTIFACTSTAGINGDIRECTORY}/Linux-${distribution}"
 		if [ "${distribution}" == "Fedora-34" ] || [ "${distribution}" == "Fedora-28" ] || [ "${distribution}" == "CentOS-7" ]; then
