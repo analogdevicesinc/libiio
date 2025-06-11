62bf9398ed1b1c743be0aab2644cc1b4578fed41
Dan Nechita <dan.nechita@analog.com>
2025-06-11 15:08:56 +0300

    CI: Make check_README_BUILD.sh support iio_option()
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- CI/azure/check_README_BUILD.sh
+++ CI/azure/check_README_BUILD.sh
@@ -18,13 +18,20 @@ for opt in $(options)
 do
 	default=$(for file in $(find ./ -not \( -path ./deps -prune \) -name CMakeLists.txt)
 	do
-		grep "option[[:space:]]*(${opt} " "${file}" | \
-			sed -e "s/^[[:space:]]*//g" -e "s/)[[:space:]]*$//" | \
-			awk '{print $NF}'
+		grep -E "(^|\s)(option|iio_option)[[:space:]]*\([[:space:]]*${opt}[[:space:]]+" "${file}" | \
+        sed -e "s/^[[:space:]]*//g" -e "s/)[[:space:]]*$//" | \
+        awk '{
+            for(i=1;i<=NF;i++) {
+                if ($i == "ON" || $i == "OFF") {
+                    print $i
+                    break
+                }
+            }
+        }'
 	done)
 	if ! grep -q "${opt}.*${default}" README_BUILD.md ; then
 		echo "no match with ${opt} set with ${default}"
-		grep -R "${opt}" ./*
+		# grep -R "${opt}" ./*
 		error=1
 	fi
 done
@@ -37,14 +44,14 @@ do
         a=$(grep -i "toggle_iio_feature.*${i}" $(find ./ -not \( -path ./deps -prune \) -name CMakeLists.txt))
         if [ -z "${a}" ] ; then
             # Check if the option is appended to OPTIONS_LISTS
-            b=$(awk -v opt="$i" '
+            awk -v opt="$i" '
                 /list[[:space:]]*\([[:space:]]*APPEND[[:space:]]+OPTIONS_LISTS/ {inlist=1}
                 inlist {
                     if ($0 ~ /\)/) inlist=0
                     if (index($0, opt)) found=1
                 }
                 END {exit !found}
-            ' $(find ./ -not \( -path ./deps -prune \) -name CMakeLists.txt))
+            ' $(find ./ -not \( -path ./deps -prune \) -name CMakeLists.txt)
             if [ $? -ne 0 ]; then
                 echo "${f} defines \"${i}\" as option, but it is missing toggle_iio_feature and is not appended to OPTIONS_LISTS"
                 error=1
