cc3c580bb36887a63f783970188758c95658c8d4
Cristina Suteu <cristina.suteu@analog.com>
2025-07-02 10:31:28 +0300

    Deploy autogenerated docs for libiio v1.0-g6a6c502
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

---
+++ main/csharp/search/variables_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['timeoutms_126',['TimeoutMs',['../structiio_1_1IioContextParams.html#a90d50aff2dba39a7ca994f54b29b3812',1,'iio::IioContextParams']]],
+  ['timestamplevel_127',['TimestampLevel',['../structiio_1_1IioContextParams.html#a3a373ad18639fc8ff91500a8c2240875',1,'iio::IioContextParams']]]
+];
--- master/csharp/search/all_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hwmon_40',['hwmon',['../classiio_1_1Device.html#a0c8992763f1c21715d03775a0e37321e',1,'iio::Device']]]
-];
