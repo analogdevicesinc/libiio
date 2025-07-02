bff4ad9cb7a6852429f6ec4f7f9a43df555d88ba
Cristina Suteu <cristina.suteu@analog.com>
2025-07-02 10:31:28 +0300

    Deploy autogenerated docs for libiio v1.0-gccc184b
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

---
+++ v1.0/csharp/search/enums_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['eventdirection_114',['EventDirection',['../classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477',1,'iio::IIOEvent']]],
+  ['eventtype_115',['EventType',['../classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7a',1,'iio::IIOEvent']]]
+];
