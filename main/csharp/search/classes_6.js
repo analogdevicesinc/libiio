8a8ed594f0090f6d2f97eb79d1b0392bf22bf62b
Cristina Suteu <cristina.suteu@analog.com>
2025-07-02 10:31:28 +0300

    Deploy autogenerated docs for libiio v1.0-g8a2c6bc
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

---
+++ main/csharp/search/classes_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['trigger_68',['Trigger',['../classiio_1_1Trigger.html',1,'iio']]]
+];
--- master/csharp/search/variables_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['library_5fversion_132',['library_version',['../classiio_1_1Context.html#abbd75cb9945ab472fdf95a02c4657354',1,'iio::Context']]]
-];
