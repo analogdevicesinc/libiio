0240659d5d374d7ac2a2adb582a0620b41268c4b
Cristina Suteu <cristina.suteu@analog.com>
2025-07-02 10:31:28 +0300

    Deploy autogenerated docs for libiio v1.0-g6d64df5
    Signed-off-by: Autogenerated by Azure Pipelines <cse-ci-notifications@analog.com>

---
+++ main/csharp/classiio_1_1IIOEvent.js
@@ -0,0 +1,29 @@
+var classiio_1_1IIOEvent =
+[
+    [ "EventDirection", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477", [
+      [ "IIO_EV_DIR_EITHER", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477a1078b8a35be8af98dce03f39bd0aa6ca", null ],
+      [ "IIO_EV_DIR_RISING", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477a526bfc99304e93bb2ad9e76babd681a9", null ],
+      [ "IIO_EV_DIR_FALLING", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477aa5fe6e0172f9dc1877a6ec097d2e74ed", null ],
+      [ "IIO_EV_DIR_NONE", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477ad6e8753456ad607d925b6a440b1dfa0e", null ],
+      [ "IIO_EV_DIR_SINGLETAP", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477aab0b6799f8c419103c794a595070f156", null ],
+      [ "IIO_EV_DIR_DOUBLETAP", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477a192fe7d5ae912f410a625f507e1d16be", null ],
+      [ "IIO_EV_DIR_FAULT_OPENWIRE", "classiio_1_1IIOEvent.html#a66f636f06d32f69bb2c41a3dd83cf477a1f44193727fd385bfca9f448bdf11869", null ]
+    ] ],
+    [ "EventType", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7a", [
+      [ "IIO_EV_TYPE_THRESH", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aabc14a1e5ff1199f86a81398a48303a81", null ],
+      [ "IIO_EV_TYPE_MAG", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aae4c879944d79fdd0152c5e3dd22ab272", null ],
+      [ "IIO_EV_TYPE_ROC", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aaaa819e3023f6a953cd696fe26994f3c2", null ],
+      [ "IIO_EV_TYPE_THRESH_ADAPTIVE", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aacc3ce53cb19b34e679ec5015a239696e", null ],
+      [ "IIO_EV_TYPE_MAG_ADAPTIVE", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aa22c21a8d349c631c8268145f93834bc7", null ],
+      [ "IIO_EV_TYPE_CHANGE", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aaf46e0a0648e2b5f65c833e52e54c4d81", null ],
+      [ "IIO_EV_TYPE_MAG_REFERENCED", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aab7795a49292de69cccf7e778cd151725", null ],
+      [ "IIO_EV_TYPE_GESTURE", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aae668f17fc8d09e51c8858e149a9387eb", null ],
+      [ "IIO_EV_TYPE_FAULT", "classiio_1_1IIOEvent.html#a9aa08de3f41137452aede16f73196a7aabcde29c93d30983ca9d5d3a8ccf260b7", null ]
+    ] ],
+    [ "chn", "classiio_1_1IIOEvent.html#a6ae09b6cfda32efd575d98d18af01bfb", null ],
+    [ "chn_diff", "classiio_1_1IIOEvent.html#af18511ddf13d98730e5251b588a09ef1", null ],
+    [ "id", "classiio_1_1IIOEvent.html#a83db1df7b3e4a7e6b8580482a4539798", null ],
+    [ "timestamp", "classiio_1_1IIOEvent.html#aabeea82e97d330e17c096a2f3c0c2a00", null ],
+    [ "direction", "classiio_1_1IIOEvent.html#af6e07c0046564a567e6eff0042dd0fb0", null ],
+    [ "type", "classiio_1_1IIOEvent.html#afb81e8437b0038efe7490188dab54f37", null ]
+];
