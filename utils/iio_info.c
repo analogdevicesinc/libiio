dc1522b8d570b26a098a28d0bdee890943e1b6d8
Dan Nechita <dan.nechita@analog.com>
2025-09-05 14:12:06 +0300

    Revert "utils: update call iio_device_create_buffer()"
    This reverts commit 95dc678dc2f7c604de4ab11a7eb9043e06bc640f.

--- utils/iio_info.c
+++ utils/iio_info.c
@@ -368,7 +368,7 @@ int main(int argc, char **argv)
 		}
 
 		if (mask)
-			buffer = iio_device_create_buffer(dev, NULL, mask);
+			buffer = iio_device_create_buffer(dev, 0, mask);
 		if (mask && !iio_err(buffer)) {
 			nb_attrs = iio_buffer_get_attrs_count(buffer);
 			if (nb_attrs)
