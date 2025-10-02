daeae9c10ec949b4e3422b939e5f833d95b641d3
Dan Nechita <dan.nechita@analog.com>
2025-10-02 18:51:07 +0300

    CI: Update name of the libxml dll used in the MinGW job
    The libxml dll that is install on msys2 via pacman has changed.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- CI/publish_deps.ps1
+++ CI/publish_deps.ps1
@@ -5,7 +5,7 @@ $ErrorView = "NormalView"
 if ("$Env:COMPILER" -eq "MinGW Makefiles") {
 	cp C:\msys64\mingw64\bin\libserialport-0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libusb-1.0.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
-	cp C:\msys64\mingw64\bin\libxml2-2.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+	cp C:\msys64\mingw64\bin\libxml2-16.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libzstd.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libgcc_s_seh-1.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 	cp C:\msys64\mingw64\bin\libiconv-2.dll $env:BUILD_ARTIFACTSTAGINGDIRECTORY
