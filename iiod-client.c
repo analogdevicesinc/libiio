79f9435ad6fb4bee4f8125fffd2b5da0249430e1
Dan Nechita <dan.nechita@analog.com>
2026-05-15 14:43:24 +0300

    Add IIOD protocol support for event attributes
    Extend the IIOD binary protocol to support reading and writing event
    attributes for both devices and channels.
    
    Add four new protocol opcodes:
    - IIOD_OP_READ_DEV_EVT_ATTR: Read device event attributes
    - IIOD_OP_WRITE_DEV_EVT_ATTR: Write device event attributes
    - IIOD_OP_READ_CHN_EVT_ATTR: Read channel event attributes
    - IIOD_OP_WRITE_CHN_EVT_ATTR: Write channel event attributes
    
    Client-side changes (iiod-client.c):
    - Add separate switch cases for IIO_ATTR_TYPE_DEVICE_EVENT and
      IIO_ATTR_TYPE_CHANNEL_EVENT in read/write operations
    - For device event attributes: Pass device index and attribute index,
      use IIOD_OP_READ/WRITE_DEV_EVT_ATTR opcodes
    - For channel event attributes: Pass device index, channel index, and
      attribute index, use IIOD_OP_READ/WRITE_CHN_EVT_ATTR opcodes
    - Legacy text protocol returns -ENOSYS for event attributes (not
      supported in older protocol version)
    
    Server-side changes (iiod-responder.h, iiod/responder.c):
    - Define the four new opcodes in the protocol header
    - Map opcodes to existing attribute read/write infrastructure in
      the responder
    
    This enables remote access to event attributes through the IIOD
    binary protocol.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- iiod-client.c
+++ iiod-client.c
@@ -547,7 +547,6 @@ static ssize_t iiod_client_read_attr_new(struct iiod_client *client,
 	case IIO_ATTR_TYPE_CHANNEL:
 		chn = attr->iio.chn;
 		dev = iio_channel_get_device(chn);
-		cmd.op = IIOD_OP_READ_CHN_ATTR;
 
 		for (i = 0; i < iio_device_get_channels_count(dev); i++)
 			if (iio_device_get_channel(dev, i) == chn)
@@ -555,11 +554,34 @@ static ssize_t iiod_client_read_attr_new(struct iiod_client *client,
 
 		arg2 = (uint16_t) i;
 
+		/* Regular channel attributes */
+		cmd.op = IIOD_OP_READ_CHN_ATTR;
+
 		for (i = 0; i < iio_channel_get_attrs_count(chn); i++)
 			if (iio_channel_get_attr(chn, i) == attr)
 				break;
 
 		if (i == iio_channel_get_attrs_count(chn))
+			return -ENOENT;
+
+		arg1 = (uint16_t) i;
+		break;
+	case IIO_ATTR_TYPE_CHANNEL_EVENT:
+		chn = attr->iio.chn;
+		dev = iio_channel_get_device(chn);
+		cmd.op = IIOD_OP_READ_CHN_EVT_ATTR;
+
+		for (i = 0; i < iio_device_get_channels_count(dev); i++)
+			if (iio_device_get_channel(dev, i) == chn)
+				break;
+
+		arg2 = (uint16_t) i;
+
+		for (i = 0; i < iio_channel_get_event_attrs_count(chn); i++)
+			if (iio_channel_get_event_attr(chn, i) == attr)
+				break;
+
+		if (i == iio_channel_get_event_attrs_count(chn))
 			return -ENOENT;
 
 		arg1 = (uint16_t) i;
@@ -573,6 +595,19 @@ static ssize_t iiod_client_read_attr_new(struct iiod_client *client,
 				break;
 
 		if (i == iio_device_get_attrs_count(dev))
+			return -ENOENT;
+
+		arg1 = (uint16_t) i;
+		break;
+	case IIO_ATTR_TYPE_DEVICE_EVENT:
+		dev = attr->iio.dev;
+		cmd.op = IIOD_OP_READ_DEV_EVT_ATTR;
+
+		for (i = 0; i < iio_device_get_event_attrs_count(dev); i++)
+			if (iio_device_get_event_attr(dev, i) == attr)
+				break;
+
+		if (i == iio_device_get_event_attrs_count(dev))
 			return -ENOENT;
 
 		arg1 = (uint16_t) i;
@@ -658,6 +693,10 @@ ssize_t iiod_client_attr_read(struct iiod_client *client,
 		iio_snprintf(buf, sizeof(buf), "READ %s DEBUG %s\r\n",
 			     id, attr->name);
 		break;
+	case IIO_ATTR_TYPE_DEVICE_EVENT:
+	case IIO_ATTR_TYPE_CHANNEL_EVENT:
+		/* Event attributes not supported in legacy text protocol */
+		return -ENOSYS;
 	case IIO_ATTR_TYPE_BUFFER:
 		iio_snprintf(buf, sizeof(buf), "READ %s BUFFER %s\r\n",
 			     id, attr->name);
@@ -713,7 +752,6 @@ static ssize_t iiod_client_write_attr_new(struct iiod_client *client,
 	case IIO_ATTR_TYPE_CHANNEL:
 		chn = attr->iio.chn;
 		dev = iio_channel_get_device(chn);
-		cmd.op = IIOD_OP_WRITE_CHN_ATTR;
 
 		for (i = 0; i < iio_device_get_channels_count(dev); i++)
 			if (iio_device_get_channel(dev, i) == chn)
@@ -721,11 +759,34 @@ static ssize_t iiod_client_write_attr_new(struct iiod_client *client,
 
 		arg2 = (uint16_t) i;
 
+		/* Regular channel attributes */
+		cmd.op = IIOD_OP_WRITE_CHN_ATTR;
+
 		for (i = 0; i < iio_channel_get_attrs_count(chn); i++)
 			if (iio_channel_get_attr(chn, i) == attr)
 				break;
 
 		if (i == iio_channel_get_attrs_count(chn))
+			return -ENOENT;
+
+		arg1 = (uint16_t) i;
+		break;
+	case IIO_ATTR_TYPE_CHANNEL_EVENT:
+		chn = attr->iio.chn;
+		dev = iio_channel_get_device(chn);
+		cmd.op = IIOD_OP_WRITE_CHN_EVT_ATTR;
+
+		for (i = 0; i < iio_device_get_channels_count(dev); i++)
+			if (iio_device_get_channel(dev, i) == chn)
+				break;
+
+		arg2 = (uint16_t) i;
+
+		for (i = 0; i < iio_channel_get_event_attrs_count(chn); i++)
+			if (iio_channel_get_event_attr(chn, i) == attr)
+				break;
+
+		if (i == iio_channel_get_event_attrs_count(chn))
 			return -ENOENT;
 
 		arg1 = (uint16_t) i;
@@ -739,6 +800,19 @@ static ssize_t iiod_client_write_attr_new(struct iiod_client *client,
 				break;
 
 		if (i == iio_device_get_attrs_count(dev))
+			return -ENOENT;
+
+		arg1 = (uint16_t) i;
+		break;
+	case IIO_ATTR_TYPE_DEVICE_EVENT:
+		dev = attr->iio.dev;
+		cmd.op = IIOD_OP_WRITE_DEV_EVT_ATTR;
+
+		for (i = 0; i < iio_device_get_event_attrs_count(dev); i++)
+			if (iio_device_get_event_attr(dev, i) == attr)
+				break;
+
+		if (i == iio_device_get_event_attrs_count(dev))
 			return -ENOENT;
 
 		arg1 = (uint16_t) i;
@@ -836,6 +910,10 @@ ssize_t iiod_client_attr_write(struct iiod_client *client,
 		iio_snprintf(buf, sizeof(buf), "WRITE %s DEBUG %s %lu\r\n",
 			     id, attr->name, (unsigned long) len);
 		break;
+	case IIO_ATTR_TYPE_DEVICE_EVENT:
+	case IIO_ATTR_TYPE_CHANNEL_EVENT:
+		/* Event attributes not supported in legacy text protocol */
+		return -ENOSYS;
 	case IIO_ATTR_TYPE_BUFFER:
 		iio_snprintf(buf, sizeof(buf), "WRITE %s BUFFER %s %lu\r\n",
 			     id, attr->name, (unsigned long) len);
