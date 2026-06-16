3baae16f3e2ef3627dbd2b619c778fcab6791a9c
PraveenP-ADI <praveen.perera@analog.com>
2026-06-16 19:17:50 +0000

    AckV Logic
    Added the rest of the logic for validating the commands and generating then sending an AckV Packet.

--- iiod/vita49_2/vita49_2_client.c
+++ iiod/vita49_2/vita49_2_client.c
@@ -464,6 +464,13 @@ static void vita49_2_main(struct thread_pool *pool, void *args)
 	// Remaining Fields that need to be set:
 	// AckV Packet:
 		// Command Prologue:
+			// Common Prologue
+				// Header:
+					// packet_size_words (this gets handled automatically when the vita49_2_generate_<packet_name>() function gets called) *
+					// packet_count *
+				// stream_id *
+				// timestamp_int *
+				// TODO: timestamp_frac, Linux doesn't provide a way to get picosecond accuracy natively
 			// CAM
 				// Warnings Present *
 				// Control Fields *
@@ -477,6 +484,8 @@ static void vita49_2_main(struct thread_pool *pool, void *args)
 		// CIF7 Warnings (if applicable) *
 		// Warnings Payload *
 		// Warnings Payload Size *
+
+	uint16_t ackV_packet_size = 0;
 
 	// ==============================================================
 	// RECEIVE LOOP
@@ -741,13 +750,75 @@ static void vita49_2_main(struct thread_pool *pool, void *args)
 						else
 						{
 							// Populating the rest of the AckV Packet
+							ackV_packet.command_prologue.common_prologue.timestamp_int = (uint32_t)(time(NULL));
 
+							stream_entry.host_ip_addr = sender_address.sin_addr.s_addr;
+							stream_entry.host_port = sender_address.sin_port;
+							stream_entry.packet_class_code = VITA49_2_PKT_CLASS_ACKV_ACKX;
+							stream_entry.stream_id = next_ackx_ackv_stream_id;
 
+							ssize_t ret_value;
+
+							// Error occurred
+							if ((ret_value = insert_stream_id(device_stream_id_table, sizeof(device_stream_id_table)/sizeof(device_stream_id_table[0]), &stream_entry) < 0))
+							{
+								fprintf(stderr, "vita49_2_client: Encountered an error while trying to retrieve Stream ID for the AckV Packet that was to be sent.\n");
+								continue;
+							}
+
+							// If the return value is less than or equal to the last_insertion_index, then that means the element already existed in the array.
+							// Otherwise that means we just inserted a new element.
+							if (ret_value > last_insertion_index)
+							{
+								last_insertion_index++;
+								next_time_data_stream_id++;
+
+								control_packet.command_prologue.common_prologue.stream_id = stream_entry.stream_id;
+								control_packet.command_prologue.common_prologue.header.packet_count = 1;
+							
+								device_stream_id_table[ret_value].packet_count = 1;
+							}
+							else
+							{
+								control_packet.command_prologue.common_prologue.stream_id = device_stream_id_table[ret_value].stream_id;
+								control_packet.command_prologue.common_prologue.header.packet_count = ++device_stream_id_table[ret_value].packet_count;
+							}
+
+							ackV_packet.command_prologue.message_id = control_packet.command_prologue.message_id;
+
+							// Determining if we should assert the Warnings Present bit in the CAM
+							if (ackV_packet.warnings_payload_num_words > 0)
+								ackV_packet.command_prologue.ack_cam->warnings_present = 1;
+
+							// Copying the 11 bits from the Control Packet CAM. To avoid any complications with byte ordering I'll manually do the copies:
+							ackV_packet.command_prologue.ack_cam->reserved_21 = 0;
+							ackV_packet.command_prologue.ack_cam->nack 					= control_packet.command_prologue.control_cam->nack;
+							ackV_packet.command_prologue.ack_cam->action_bits 			= control_packet.command_prologue.control_cam->action_bits;
+							ackV_packet.command_prologue.ack_cam->errors				= control_packet.command_prologue.control_cam->errors;
+							ackV_packet.command_prologue.ack_cam->warnings				= control_packet.command_prologue.control_cam->warnings;
+							ackV_packet.command_prologue.ack_cam->partial_execution		= control_packet.command_prologue.control_cam->partial_execution;
+							ackV_packet.command_prologue.ack_cam->controller_id_format	= control_packet.command_prologue.control_cam->controller_id_format;
+							ackV_packet.command_prologue.ack_cam->has_controller_id		= control_packet.command_prologue.control_cam->has_controller_id;
+							ackV_packet.command_prologue.ack_cam->controllee_id_format	= control_packet.command_prologue.control_cam->controllee_id_format;
+							ackV_packet.command_prologue.ack_cam->has_controllee_id		= control_packet.command_prologue.control_cam->has_controllee_id;
+
+							// Copying the Controller ID information from the Control Packet
+							memcpy(&ackV_packet.command_prologue.controller_id, &control_packet.command_prologue.controller_id, sizeof(ackV_packet.command_prologue.controller_id));
+						
+							// Now to write that data to a buffer and send it
+							if ((ackV_packet_size = vita49_2_generate_ackv_packet(&ackV_packet, &send_buffer, sizeof(send_buffer)/4)) < 0)
+							{
+								fprintf(stderr, "vita49_2_client: Failed to serialize AckV Packet.\n");
+								continue;							
+							}
+
+							if (sendto(socket_fd, send_buffer, ackV_packet_size*4, 0, (struct sockaddr*)&sender_address, sizeof(sender_address)) <= 0)
+								fprintf(stderr, "vita49_2_client: Failed to send AckV Packet over UDP.\n");
 						}
 					}
 
-					// Executing the commands in the packet. Currently ADI only supports Execute Mode and No-Action Mode for Control Packets,
-				// however we retain the right to implement Dry Run Mode in the future.
+					// Now to execute the commands in the packet. Currently ADI only supports Execute Mode and No-Action Mode for Control Packets,
+					// however we retain the right to implement Dry Run Mode in the future.
 
 					// TODO: Add logic that looks at the timestamp and schedules execution of the controls in the future.
 					// An idea might be to spawn a detached pthread that checks a timer_fd and executes the controls once the timer
@@ -1719,6 +1790,18 @@ int validate_commands(struct iio_context *ctx, const struct vita49_2_control_pac
 
 			// TODO: Logic to support CIF1/2/3/7 as well
 		}
+	}
+
+	// Now to write the warnings (if any) to the AckV Packet's payload
+	if (cif0_warnings_index > 0)
+	{
+		ackV_packet->warnings_payload = calloc(cif0_warnings_index, sizeof(struct vita49_2_warning_error_indicators));
+		if (ackV_packet->warnings_payload == NULL)
+			return -ENOMEM;
+
+		memcpy(&ackV_packet->warnings_payload, &cif0_warnings, sizeof(cif0_warnings));
+
+		ackV_packet->warnings_payload_num_words = cif0_warnings_index;
 	}
 
 	return 0;
