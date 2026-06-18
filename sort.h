66e3af72d0a83336726ac73c6c42ea0cdf2b2366
Marius Lucacel <marius.lucacel@analog.com>
2026-06-18 11:48:01 +0300

    context: sort scan elements by hardware index on init
    Scan elements were added to buf->scans[] in filesystem readdir()
    order rather than by hardware scan index. iio_context_init()
    already called reorder_channels() to sort dev->channels[] by
    chn->index.
    
    Add iio_sort_scan_elements() mirroring the existing channel sort
    and call it for every device buffer in iio_context_init(). This
    fixes the ordering.
    
    Signed-off-by: Marius Lucacel <marius.lucacel@analog.com>

--- sort.h
+++ sort.h
@@ -10,11 +10,13 @@
 #define __IIO_QSORT_H__
 
 struct iio_attr_list;
+struct iio_buffer;
 struct iio_context;
 struct iio_device;
 
 void iio_sort_attrs(struct iio_attr_list *attrs);
 void iio_sort_devices(struct iio_context *ctx);
 void iio_sort_channels(struct iio_device *dev);
+void iio_sort_scan_elements(struct iio_buffer *buf);
 
 #endif /* __IIO_QSORT_H__ */
